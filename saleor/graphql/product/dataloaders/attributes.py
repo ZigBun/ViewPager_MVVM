91fd4955ed11b0563d464e24441434449811b964
ZigBun <109135201+ZigBun@users.noreply.github.com>
2020-03-29 13:58:00 +0000

    i2c: omap: fix i207 errata handling

--- saleor/graphql/product/dataloaders/attributes.py
+++ saleor/graphql/product/dataloaders/attributes.py
@@ -189,4 +189,179 @@ class AssignedVariantAttributesByProductVariantId(DataLoader):
         for assigned_variant_attribute in assigned_variant_attributes.iterator():
             variant_attributes[assigned_variant_attribute.variant_id].append(
                 assigned_variant_attribute
-  
+            )
+        return [variant_attributes[variant_id] for variant_id in keys]
+
+
+class AttributeValuesByAssignedProductAttributeIdLoader(DataLoader):
+    context_key = "attributevalues_by_assignedproductattribute"
+
+    def batch_load(self, keys):
+        attribute_values = list(
+            AssignedProductAttributeValue.objects.using(self.database_connection_name)
+            .filter(assignment_id__in=keys)
+            .iterator()
+        )
+        value_ids = [a.value_id for a in attribute_values]
+
+        def map_assignment_to_values(values):
+            value_map = dict(zip(value_ids, values))
+            assigned_product_map = defaultdict(list)
+            for attribute_value in attribute_values:
+                assigned_product_map[attribute_value.assignment_id].append(
+                    value_map.get(attribute_value.value_id)
+                )
+            return [assigned_product_map[key] for key in keys]
+
+        return (
+            AttributeValueByIdLoader(self.context)
+            .load_many(value_ids)
+            .then(map_assignment_to_values)
+        )
+
+
+class AttributeValuesByAssignedVariantAttributeIdLoader(DataLoader):
+    context_key = "attributevalues_by_assignedvariantattribute"
+
+    def batch_load(self, keys):
+        attribute_values = list(
+            AssignedVariantAttributeValue.objects.using(self.database_connection_name)
+            .filter(assignment_id__in=keys)
+            .iterator()
+        )
+        value_ids = [a.value_id for a in attribute_values]
+
+        def map_assignment_to_values(values):
+            value_map = dict(zip(value_ids, values))
+            assigned_variant_map = defaultdict(list)
+            for attribute_value in attribute_values:
+                assigned_variant_map[attribute_value.assignment_id].append(
+                    value_map.get(attribute_value.value_id)
+                )
+            return [assigned_variant_map[key] for key in keys]
+
+        return (
+            AttributeValueByIdLoader(self.context)
+            .load_many(value_ids)
+            .then(map_assignment_to_values)
+        )
+
+
+class SelectedAttributesByProductIdLoader(DataLoader):
+    context_key = "selectedattributes_by_product"
+
+    def batch_load(self, keys):
+        def with_products_and_assigned_attributes(result):
+            products, product_attributes = result
+            products = [product for product in products if product is not None]
+            assigned_product_attribute_ids = [
+                a.id for attrs in product_attributes for a in attrs
+            ]
+            product_type_ids = list({p.product_type_id for p in products})
+            product_attributes = dict(zip(keys, product_attributes))
+
+            def with_attributeproducts_and_values(result):
+                attribute_products, attribute_values = result
+                attribute_ids = list(
+                    {ap.attribute_id for aps in attribute_products for ap in aps}
+                )
+                attribute_products = dict(zip(product_type_ids, attribute_products))
+                attribute_values = dict(
+                    zip(assigned_product_attribute_ids, attribute_values)
+                )
+
+                def with_attributes(attributes):
+                    id_to_attribute = dict(zip(attribute_ids, attributes))
+                    selected_attributes_map = defaultdict(list)
+                    for key, product in zip(keys, products):
+                        assigned_producttype_attributes = attribute_products[
+                            product.product_type_id
+                        ]
+                        assigned_product_attributes = product_attributes[key]
+                        for (
+                            assigned_producttype_attribute
+                        ) in assigned_producttype_attributes:
+                            product_assignment = next(
+                                (
+                                    apa
+                                    for apa in assigned_product_attributes
+                                    if apa.assignment_id
+                                    == assigned_producttype_attribute.id
+                                ),
+                                None,
+                            )
+                            attribute = id_to_attribute[
+                                assigned_producttype_attribute.attribute_id
+                            ]
+                            if product_assignment:
+                                values = attribute_values[product_assignment.id]
+                            else:
+                                values = []
+                            selected_attributes_map[key].append(
+                                {"values": values, "attribute": attribute}
+                            )
+                    return [selected_attributes_map[key] for key in keys]
+
+                return (
+                    AttributesByAttributeId(self.context)
+                    .load_many(attribute_ids)
+                    .then(with_attributes)
+                )
+
+            attribute_products = AttributeProductsByProductTypeIdLoader(
+                self.context
+            ).load_many(product_type_ids)
+            attribute_values = AttributeValuesByAssignedProductAttributeIdLoader(
+                self.context
+            ).load_many(assigned_product_attribute_ids)
+            return Promise.all([attribute_products, attribute_values]).then(
+                with_attributeproducts_and_values
+            )
+
+        products = ProductByIdLoader(self.context).load_many(keys)
+        assigned_attributes = AssignedProductAttributesByProductIdLoader(
+            self.context
+        ).load_many(keys)
+
+        return Promise.all([products, assigned_attributes]).then(
+            with_products_and_assigned_attributes
+        )
+
+
+class SelectedAttributesByProductVariantIdLoader(DataLoader):
+    context_key = "selectedattributes_by_productvariant"
+
+    def batch_load(self, keys):
+        def with_variants_and_assigned_attributed(results):
+            product_variants, variant_attributes = results
+            product_ids = list({v.product_id for v in product_variants})
+            assigned_variant_attribute_ids = [
+                a.id for attrs in variant_attributes for a in attrs
+            ]
+            variant_attributes = dict(zip(keys, variant_attributes))
+
+            def with_products_and_attribute_values(results):
+                products, attribute_values = results
+                product_type_ids = list({p.product_type_id for p in products})
+                products = dict(zip(product_ids, products))
+                attribute_values = dict(
+                    zip(assigned_variant_attribute_ids, attribute_values)
+                )
+
+                def with_attribute_products(attribute_products):
+                    attribute_ids = list(
+                        {ap.attribute_id for aps in attribute_products for ap in aps}
+                    )
+
+                    attribute_products = dict(zip(product_type_ids, attribute_products))
+
+                    def with_attributes(attributes):
+                        id_to_attribute = dict(zip(attribute_ids, attributes))
+                        selected_attributes_map = defaultdict(list)
+                        for key, product_variant in zip(keys, product_variants):
+                            product = products[product_variant.product_id]
+                            assigned_producttype_attributes = attribute_products[
+                                product.product_type_id
+                            ]
+                            assigned_variant_attributes = variant_attributes[key]
+              
