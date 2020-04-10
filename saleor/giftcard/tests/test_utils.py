fe845e5f24cf029d9a9526d1d2dcffaa055af36f
ZigBun <109135201+ZigBun@users.noreply.github.com>
2020-04-10 15:53:11 +0000

    Update to latest libprimesieve

---
+++ saleor/giftcard/tests/test_utils.py
@@ -0,0 +1,209 @@
+import json
+from datetime import date, timedelta
+from unittest.mock import patch
+
+import graphene
+import pytest
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
+from freezegun import freeze_time
+
+from ...core import TimePeriodType
+from ...core.exceptions import GiftCardNotApplicable
+from ...core.utils.json_serializer import CustomJsonEncoder
+from ...core.utils.promo_code import InvalidPromoCode
+from ...order.models import OrderLine
+from ...plugins.manager import get_plugins_manager
+from ...site import GiftCardSettingsExpiryType
+from ...tests.utils import flush_post_commit_hooks
+from ...webhook.event_types import WebhookEventAsyncType
+from ...webhook.payloads import generate_meta, generate_requestor
+from .. import GiftCardEvents, GiftCardLineData, events
+from ..models import GiftCard, GiftCardEvent
+from ..utils import (
+    add_gift_card_code_to_checkout,
+    assign_user_gift_cards,
+    calculate_expiry_date,
+    deactivate_order_gift_cards,
+    fulfill_gift_card_lines,
+    fulfill_non_shippable_gift_cards,
+    get_gift_card_lines,
+    get_non_shippable_gift_card_lines,
+    gift_cards_create,
+    is_gift_card_expired,
+    order_has_gift_card_lines,
+    remove_gift_card_code_from_checkout,
+)
+
+
+def test_add_gift_card_code_to_checkout(checkout, gift_card):
+    # given
+    assert checkout.gift_cards.count() == 0
+
+    # when
+    add_gift_card_code_to_checkout(
+        checkout, "test@example.com", gift_card.code, gift_card.currency
+    )
+
+    # then
+    assert checkout.gift_cards.count() == 1
+
+
+def test_add_gift_card_code_to_checkout_inactive_card(checkout, gift_card):
+    # given
+    gift_card.is_active = False
+    gift_card.save(update_fields=["is_active"])
+
+    assert checkout.gift_cards.count() == 0
+
+    # when
+    # then
+    with pytest.raises(InvalidPromoCode):
+        add_gift_card_code_to_checkout(
+            checkout, "test@example.com", gift_card.code, gift_card.currency
+        )
+
+
+def test_add_gift_card_code_to_checkout_expired_card(checkout, gift_card):
+    # given
+    gift_card.expiry_date = date.today() - timedelta(days=10)
+    gift_card.save(update_fields=["expiry_date"])
+
+    assert checkout.gift_cards.count() == 0
+
+    # when
+    # then
+    with pytest.raises(InvalidPromoCode):
+        add_gift_card_code_to_checkout(
+            checkout, "test@example.com", gift_card.code, gift_card.currency
+        )
+
+
+def test_add_gift_card_code_to_checkout_invalid_currency(checkout, gift_card):
+    # given
+    currency = "EUR"
+
+    assert gift_card.currency != currency
+    assert checkout.gift_cards.count() == 0
+
+    # when
+    # then
+    with pytest.raises(InvalidPromoCode):
+        add_gift_card_code_to_checkout(
+            checkout, "test@example.com", gift_card.code, currency
+        )
+
+
+def test_add_gift_card_code_to_checkout_used_gift_card(checkout, gift_card_used):
+    # given
+    assert gift_card_used.used_by_email
+    assert checkout.gift_cards.count() == 0
+
+    # when
+    add_gift_card_code_to_checkout(
+        checkout,
+        gift_card_used.used_by_email,
+        gift_card_used.code,
+        gift_card_used.currency,
+    )
+
+    # then
+    assert checkout.gift_cards.count() == 1
+
+
+def test_add_gift_card_code_to_checkout_used_gift_card_invalid_user(
+    checkout, gift_card_used
+):
+    # given
+    email = "new_user@example.com"
+    assert gift_card_used.used_by_email
+    assert gift_card_used.used_by_email != email
+    assert checkout.gift_cards.count() == 0
+
+    # when
+    # then
+    with pytest.raises(InvalidPromoCode):
+        add_gift_card_code_to_checkout(
+            checkout, email, gift_card_used.code, gift_card_used.currency
+        )
+
+
+def test_remove_gift_card_code_from_checkout(checkout, gift_card):
+    # given
+    checkout.gift_cards.add(gift_card)
+    assert checkout.gift_cards.count() == 1
+
+    # when
+    remove_gift_card_code_from_checkout(checkout, gift_card.code)
+
+    # then
+    assert checkout.gift_cards.count() == 0
+
+
+def test_remove_gift_card_code_from_checkout_no_checkout_gift_cards(
+    checkout, gift_card
+):
+    # given
+    assert checkout.gift_cards.count() == 0
+
+    # when
+    remove_gift_card_code_from_checkout(checkout, gift_card.code)
+
+    # then
+    assert checkout.gift_cards.count() == 0
+
+
+@pytest.mark.parametrize(
+    "period_type, period", [("years", 5), ("weeks", 1), ("months", 13), ("days", 100)]
+)
+def test_calculate_expiry_settings(period_type, period, site_settings):
+    # given
+    site_settings.gift_card_expiry_type = GiftCardSettingsExpiryType.EXPIRY_PERIOD
+    site_settings.gift_card_expiry_period_type = period_type.rstrip("s")
+    site_settings.gift_card_expiry_period = period
+    site_settings.save(
+        update_fields=[
+            "gift_card_expiry_type",
+            "gift_card_expiry_period_type",
+            "gift_card_expiry_period",
+        ]
+    )
+
+    # when
+    expiry_date = calculate_expiry_date(site_settings)
+
+    # then
+    assert expiry_date == timezone.now().date() + relativedelta(**{period_type: period})
+
+
+def test_calculate_expiry_settings_for_never_expire_settings(site_settings):
+    # given
+    site_settings.gift_card_expiry_type = GiftCardSettingsExpiryType.NEVER_EXPIRE
+
+    # when
+    expiry_date = calculate_expiry_date(site_settings)
+
+    # then
+    assert expiry_date is None
+
+
+@patch("saleor.giftcard.utils.send_gift_card_notification")
+def test_gift_cards_create(
+    send_notification_mock,
+    order,
+    gift_card_shippable_order_line,
+    gift_card_non_shippable_order_line,
+    site_settings,
+    staff_user,
+):
+    # given
+    manager = get_plugins_manager()
+    line_1, line_2 = gift_card_shippable_order_line, gift_card_non_shippable_order_line
+    user_email = order.user_email
+    fulfillment = order.fulfillments.create(tracking_number="123")
+    fulfillment_line_1 = fulfillment.lines.create(
+        order_line=line_1,
+        quantity=line_1.quantity,
+        stock=line_1.allocations.get().stock,
+    )
+    fulfillment_lin
