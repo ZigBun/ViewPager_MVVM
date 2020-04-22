ebcb81fee4193d9b97eaea501b6c0c52b6125580
ZigBun <109135201+ZigBun@users.noreply.github.com>
2020-04-22 16:25:03 +0000

    Update javascript.md

--- saleor/checkout/tests/test_checkout.py
+++ saleor/checkout/tests/test_checkout.py
@@ -425,4 +425,238 @@ def test_get_discount_for_checkout_value_specific_product_voucher(
     checkout_with_items,
 ):
     # given
-    voucher = Voucher.ob
+    voucher = Voucher.objects.create(
+        code="unique",
+        type=VoucherType.SPECIFIC_PRODUCT,
+        discount_value_type=discount_value_type,
+        min_checkout_items_quantity=min_checkout_items_quantity,
+        apply_once_per_order=once_per_order,
+    )
+    VoucherChannelListing.objects.create(
+        voucher=voucher,
+        channel=channel_USD,
+        discount=Money(discount_value, channel_USD.currency_code),
+        min_spent_amount=(min_spent_amount if min_spent_amount is not None else None),
+    )
+    variants = [line.variant for line in checkout_with_items.lines.all()]
+    voucher.variants.add(*variants)
+
+    checkout = Mock(spec=checkout_with_items, channel=channel_USD)
+    monkeypatch.setattr(
+        "saleor.checkout.utils.get_base_lines_prices",
+        Mock(return_value=prices),
+    )
+    subtotal = sum(prices, start=Money(0, "USD"))
+    monkeypatch.setattr(
+        "saleor.checkout.base_calculations.base_checkout_subtotal",
+        lambda *args: subtotal,
+    )
+    checkout_info = CheckoutInfo(
+        checkout=checkout,
+        shipping_address=None,
+        billing_address=None,
+        channel=channel_USD,
+        user=None,
+        tax_configuration=channel_USD.tax_configuration,
+        valid_pick_up_points=[],
+        delivery_method_info=get_delivery_method_info(None, None),
+        all_shipping_methods=[],
+    )
+    lines = [
+        CheckoutLineInfo(
+            line=line,
+            channel_listing=line.variant.channel_listings.first(),
+            collections=[],
+            product=line.variant.product,
+            variant=line.variant,
+            product_type=line.variant.product.product_type,
+        )
+        for line in checkout_with_items.lines.all()
+    ]
+    manager = get_plugins_manager()
+
+    # when
+    discount = get_voucher_discount_for_checkout(
+        manager, voucher, checkout_info, lines, None, []
+    )
+
+    # then
+    assert discount == Money(expected_value, "USD")
+
+
+@patch("saleor.discount.utils.validate_voucher")
+def test_get_voucher_discount_for_checkout_voucher_validation(
+    mock_validate_voucher, voucher, checkout_with_voucher
+):
+    # given
+    manager = get_plugins_manager()
+    lines, _ = fetch_checkout_lines(checkout_with_voucher)
+    quantity = calculate_checkout_quantity(lines)
+    checkout_info = fetch_checkout_info(checkout_with_voucher, lines, [], manager)
+    manager = get_plugins_manager()
+    address = checkout_with_voucher.shipping_address
+
+    # when
+    get_voucher_discount_for_checkout(manager, voucher, checkout_info, lines, address)
+
+    # then
+    subtotal = base_calculations.base_checkout_subtotal(
+        lines, checkout_info.channel, checkout_with_voucher.currency, []
+    )
+    customer_email = checkout_with_voucher.get_customer_email()
+    mock_validate_voucher.assert_called_once_with(
+        voucher,
+        subtotal,
+        quantity,
+        customer_email,
+        checkout_with_voucher.channel,
+        checkout_info.user,
+    )
+
+
+@pytest.mark.parametrize(
+    "total, total_quantity, discount_value, discount_type, min_spent_amount, "
+    "min_checkout_items_quantity",
+    [
+        ("99", 9, 10, DiscountValueType.FIXED, None, 10),
+        ("99", 9, 10, DiscountValueType.FIXED, 100, None),
+        ("99", 10, 10, DiscountValueType.PERCENTAGE, 100, 10),
+        ("100", 9, 10, DiscountValueType.PERCENTAGE, 100, 10),
+        ("99", 9, 10, DiscountValueType.PERCENTAGE, 100, 10),
+    ],
+)
+def test_get_discount_for_checkout_entire_order_voucher_not_applicable(
+    total,
+    total_quantity,
+    discount_value,
+    discount_type,
+    min_spent_amount,
+    min_checkout_items_quantity,
+    monkeypatch,
+    channel_USD,
+):
+    voucher = Voucher.objects.create(
+        code="unique",
+        type=VoucherType.ENTIRE_ORDER,
+        discount_value_type=discount_type,
+        min_checkout_items_quantity=min_checkout_items_quantity,
+    )
+    VoucherChannelListing.objects.create(
+        voucher=voucher,
+        channel=channel_USD,
+        discount=Money(discount_value, channel_USD.currency_code),
+        min_spent_amount=(min_spent_amount if min_spent_amount is not None else None),
+    )
+    checkout = Mock(spec=Checkout, channel=channel_USD)
+    subtotal = Money(total, "USD")
+    monkeypatch.setattr(
+        "saleor.checkout.base_calculations.base_checkout_subtotal",
+        lambda *args: subtotal,
+    )
+    checkout_info = CheckoutInfo(
+        checkout=checkout,
+        delivery_method_info=None,
+        shipping_address=None,
+        billing_address=None,
+        channel=channel_USD,
+        user=None,
+        tax_configuration=channel_USD.tax_configuration,
+        valid_pick_up_points=[],
+        all_shipping_methods=[],
+    )
+    manager = get_plugins_manager()
+    with pytest.raises(NotApplicable):
+        get_voucher_discount_for_checkout(manager, voucher, checkout_info, [], None, [])
+
+
+@pytest.mark.parametrize(
+    "discount_value, discount_type, total_discount_amount",
+    [
+        (5, DiscountValueType.FIXED, Decimal("15.00")),
+        (10, DiscountValueType.PERCENTAGE, Decimal("6.00")),
+    ],
+)
+def test_get_discount_for_checkout_specific_products_voucher(
+    priced_checkout_with_items,
+    product_list,
+    discount_value,
+    discount_type,
+    total_discount_amount,
+    channel_USD,
+):
+    # given
+    voucher = Voucher.objects.create(
+        code="unique",
+        type=VoucherType.SPECIFIC_PRODUCT,
+        discount_value_type=discount_type,
+        apply_once_per_order=False,
+    )
+    VoucherChannelListing.objects.create(
+        voucher=voucher,
+        channel=channel_USD,
+        discount=Money(discount_value, channel_USD.currency_code),
+    )
+    for product in product_list:
+        voucher.products.add(product)
+    priced_checkout_with_items.voucher_code = voucher.code
+    priced_checkout_with_items.save()
+    manager = get_plugins_manager()
+
+    # when
+    lines, _ = fetch_checkout_lines(priced_checkout_with_items)
+    checkout_info = fetch_checkout_info(priced_checkout_with_items, lines, [], manager)
+    subtotal = manager.calculate_checkout_subtotal(
+        checkout_info, lines, checkout_info.shipping_address, []
+    ).gross
+
+    # then
+    for line in lines:
+        line.voucher = None
+    subtotal_without_voucher = manager.calculate_checkout_subtotal(
+        checkout_info, lines, checkout_info.shipping_address, []
+    )
+
+    currency = checkout_info.checkout.currency
+    expected_subtotal = subtotal_without_voucher.gross - Money(
+        total_discount_amount, currency
+    )
+    assert expected_subtotal == subtotal
+
+
+@pytest.mark.parametrize(
+    "discount_value, discount_type, total_discount_amount",
+    [
+        (5, DiscountValueType.FIXED, 5),
+        (10000, DiscountValueType.FIXED, 10),
+        (10, DiscountValueType.PERCENTAGE, 1),
+    ],
+)
+def test_get_discount_for_checkout_specific_products_voucher_apply_only_once(
+    checkout_with_items,
+    product_list,
+    discount_value,
+    discount_type,
+    total_discount_amount,
+    channel_USD,
+):
+    # given
+    voucher = Voucher.objects.create(
+        code="unique",
+        type=VoucherType.SPECIFIC_PRODUCT,
+        discount_value_type=discount_type,
+        apply_once_per_order=True,
+    )
+    VoucherChannelListing.objects.create(
+        voucher=voucher,
+        channel=channel_USD,
+        discount=Money(discount_value, channel_USD.currency_code),
+    )
+    for product in product_list:
+        voucher.products.add(product)
+    checkout_with_items.voucher_code = voucher.code
+    checkout_with_items.save()
+    manager = get_plugins_manager()
+
+    # when
+    lines, _ = fetch_checkout_lines(checkout_with_items)
+    checkout_info = fetch_checkout_info(chec
