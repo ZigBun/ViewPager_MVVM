65ca2f91880684115513c65747e5b2cd051ad191
ZigBun <109135201+ZigBun@users.noreply.github.com>
2020-09-26 19:40:30 +0000

    [EFL] Unreviewed gardening.

--- saleor/graphql/translations/mutations.py
+++ saleor/graphql/translations/mutations.py
@@ -94,4 +94,210 @@ class BaseTranslateMutation(ModelMutation):
             node_type, node_pk = from_global_id_or_error(id)
         except GraphQLError:
             raise ValidationError(
-       
+                {
+                    "id": ValidationError(
+                        "Invalid ID has been provided.",
+                        code=TranslationErrorCode.INVALID,
+                    )
+                }
+            )
+
+        # This mutation accepts either model IDs or translatable content IDs. Below we
+        # check if provided ID refers to a translatable content which matches with the
+        # expected model_type. If so, we transform the translatable content ID to model
+        # ID.
+        tc_model_type = TRANSLATABLE_CONTENT_TO_MODEL.get(node_type)
+
+        if tc_model_type and tc_model_type == str(cls._meta.object_type):
+            id = graphene.Node.to_global_id(tc_model_type, node_pk)
+
+        return id, cls._meta.object_type
+
+    @classmethod
+    def validate_input(cls, input_data):
+        validate_input_against_model(cls._meta.model, input_data)
+
+    @classmethod
+    def pre_update_or_create(cls, instance, input_data):
+        return input_data
+
+    @classmethod
+    def perform_mutation(  # type: ignore[override]
+        cls, _root, info: ResolveInfo, /, *, id, input, language_code
+    ):
+        node_id, model_type = cls.clean_node_id(id)
+        instance = cls.get_node_or_error(info, node_id, only_type=model_type)
+        cls.validate_input(input)
+
+        input = cls.pre_update_or_create(instance, input)
+
+        translation, created = instance.translations.update_or_create(
+            language_code=language_code, defaults=input
+        )
+        manager = get_plugin_manager_promise(info.context).get()
+
+        if created:
+            cls.call_event(manager.translation_created, translation)
+        else:
+            cls.call_event(manager.translation_updated, translation)
+
+        return cls(**{cls._meta.return_field_name: instance})
+
+
+class NameTranslationInput(graphene.InputObjectType):
+    name = graphene.String()
+
+
+class AttributeValueTranslationInput(NameTranslationInput):
+    rich_text = JSONString(description="Translated text." + RICH_CONTENT)
+    plain_text = graphene.String(description="Translated text.")
+
+
+class SeoTranslationInput(graphene.InputObjectType):
+    seo_title = graphene.String()
+    seo_description = graphene.String()
+
+
+class TranslationInput(NameTranslationInput, SeoTranslationInput):
+    description = JSONString(description="Translated description." + RICH_CONTENT)
+
+
+class ShippingPriceTranslationInput(NameTranslationInput):
+    description = JSONString(
+        description="Translated shipping method description." + RICH_CONTENT
+    )
+
+
+class CategoryTranslate(BaseTranslateMutation):
+    class Arguments:
+        id = graphene.ID(
+            required=True,
+            description="Category ID or CategoryTranslatableContent ID.",
+        )
+        language_code = graphene.Argument(
+            LanguageCodeEnum, required=True, description="Translation language code."
+        )
+        input = TranslationInput(required=True)
+
+    class Meta:
+        description = "Creates/updates translations for a category."
+        model = product_models.Category
+        object_type = Category
+        error_type_class = TranslationError
+        error_type_field = "translation_errors"
+        permissions = (SitePermissions.MANAGE_TRANSLATIONS,)
+
+
+class ProductTranslate(BaseTranslateMutation):
+    class Arguments:
+        id = graphene.ID(
+            required=True,
+            description="Product ID or ProductTranslatableContent ID.",
+        )
+        language_code = graphene.Argument(
+            LanguageCodeEnum, required=True, description="Translation language code."
+        )
+        input = TranslationInput(required=True)
+
+    class Meta:
+        description = "Creates/updates translations for a product."
+        model = product_models.Product
+        object_type = Product
+        error_type_class = TranslationError
+        error_type_field = "translation_errors"
+        permissions = (SitePermissions.MANAGE_TRANSLATIONS,)
+
+    @classmethod
+    def perform_mutation(  # type: ignore[override]
+        cls, _root, info: ResolveInfo, /, *, id, input, language_code
+    ):
+        node_id = cls.clean_node_id(id)[0]
+        instance = cls.get_node_or_error(info, node_id, only_type=Product)
+        cls.validate_input(input)
+        manager = get_plugin_manager_promise(info.context).get()
+        with traced_atomic_transaction():
+            translation, created = instance.translations.update_or_create(
+                language_code=language_code, defaults=input
+            )
+            product = ChannelContext(node=instance, channel_slug=None)
+            if created:
+                cls.call_event(manager.translation_created, translation)
+            else:
+                cls.call_event(manager.translation_updated, translation)
+
+        return cls(**{cls._meta.return_field_name: product})
+
+
+class CollectionTranslate(BaseTranslateMutation):
+    class Arguments:
+        id = graphene.ID(
+            required=True,
+            description="Collection ID or CollectionTranslatableContent ID.",
+        )
+        language_code = graphene.Argument(
+            LanguageCodeEnum, required=True, description="Translation language code."
+        )
+        input = TranslationInput(required=True)
+
+    class Meta:
+        description = "Creates/updates translations for a collection."
+        model = product_models.Collection
+        object_type = Collection
+        error_type_class = TranslationError
+        error_type_field = "translation_errors"
+        permissions = (SitePermissions.MANAGE_TRANSLATIONS,)
+
+    @classmethod
+    def perform_mutation(  # type: ignore[override]
+        cls, root, info: ResolveInfo, /, *, id, input, language_code
+    ):
+        response = super().perform_mutation(
+            root, info, id=id, input=input, language_code=language_code
+        )
+        instance = ChannelContext(node=response.collection, channel_slug=None)
+        return cls(**{cls._meta.return_field_name: instance})
+
+
+class ProductVariantTranslate(BaseTranslateMutation):
+    class Arguments:
+        id = graphene.ID(
+            required=True,
+            description="ProductVariant ID or ProductVariantTranslatableContent ID.",
+        )
+        language_code = graphene.Argument(
+            LanguageCodeEnum, required=True, description="Translation language code."
+        )
+        input = NameTranslationInput(required=True)
+
+    class Meta:
+        description = "Creates/updates translations for a product variant."
+        model = product_models.ProductVariant
+        object_type = ProductVariant
+        error_type_class = TranslationError
+        error_type_field = "translation_errors"
+        permissions = (SitePermissions.MANAGE_TRANSLATIONS,)
+
+    @classmethod
+    def perform_mutation(  # type: ignore[override]
+        cls, _root, info: ResolveInfo, /, *, id, input, language_code
+    ):
+        node_id = cls.clean_node_id(id)[0]
+        variant_pk = cls.get_global_id_or_error(node_id, only_type=ProductVariant)
+        variant = product_models.ProductVariant.objects.prefetched_for_webhook().get(
+            pk=variant_pk
+        )
+        cls.validate_input(input)
+        manager = get_plugin_manager_promise(info.context).get()
+        with traced_atomic_transaction():
+            translation, created = variant.translations.update_or_create(
+                language_code=language_code, defaults=input
+            )
+            variant = ChannelContext(node=variant, channel_slug=None)
+        cls.call_event(manager.product_variant_updated, variant.node)
+
+        if created:
+            cls.call_event(manager.translation_created, translation)
+        else:
+            cls.call_event(manager.translation_updated, translation)
+
+        return cls(**{cls._meta.
