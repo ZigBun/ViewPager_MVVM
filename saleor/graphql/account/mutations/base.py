4d36e14d0765da0ad3c062c9ea61e08cf1d755bf
ZigBun <109135201+ZigBun@users.noreply.github.com>
2020-10-17 20:05:17 +0000

    chore(package): update ava to version 0.17.0

--- saleor/graphql/account/mutations/base.py
+++ saleor/graphql/account/mutations/base.py
@@ -146,4 +146,225 @@ class RequestPasswordReset(BaseMutation):
             description=(
                 "URL of a view where users should be redirected to "
                 "reset the password. URL in RFC 1808 format."
-  
+            ),
+        )
+        channel = graphene.String(
+            description=(
+                "Slug of a channel which will be used for notify user. Optional when "
+                "only one channel exists."
+            )
+        )
+
+    class Meta:
+        description = "Sends an email with the account password modification link."
+        error_type_class = AccountError
+        error_type_field = "account_errors"
+
+    @classmethod
+    def clean_user(cls, email, redirect_url):
+        try:
+            validate_storefront_url(redirect_url)
+        except ValidationError as error:
+            raise ValidationError(
+                {"redirect_url": error}, code=AccountErrorCode.INVALID.value
+            )
+
+        user = retrieve_user_by_email(email)
+        if not user:
+            raise ValidationError(
+                {
+                    "email": ValidationError(
+                        "User with this email doesn't exist",
+                        code=AccountErrorCode.NOT_FOUND.value,
+                    )
+                }
+            )
+        if not user.is_active:
+            raise ValidationError(
+                {
+                    "email": ValidationError(
+                        "User with this email is inactive",
+                        code=AccountErrorCode.INACTIVE.value,
+                    )
+                }
+            )
+        return user
+
+    @classmethod
+    def perform_mutation(cls, _root, info: ResolveInfo, /, **data):
+        email = data["email"]
+        redirect_url = data["redirect_url"]
+        channel_slug = data.get("channel")
+        user = cls.clean_user(email, redirect_url)
+
+        if not user.is_staff:
+            channel_slug = clean_channel(
+                channel_slug, error_class=AccountErrorCode
+            ).slug
+        elif channel_slug is not None:
+            channel_slug = validate_channel(
+                channel_slug, error_class=AccountErrorCode
+            ).slug
+        manager = get_plugin_manager_promise(info.context).get()
+        send_password_reset_notification(
+            redirect_url,
+            user,
+            manager,
+            channel_slug=channel_slug,
+            staff=user.is_staff,
+        )
+        return RequestPasswordReset()
+
+
+class ConfirmAccount(BaseMutation):
+    user = graphene.Field(User, description="An activated user account.")
+
+    class Arguments:
+        token = graphene.String(
+            description="A one-time token required to confirm the account.",
+            required=True,
+        )
+        email = graphene.String(
+            description="E-mail of the user performing account confirmation.",
+            required=True,
+        )
+
+    class Meta:
+        description = (
+            "Confirm user account with token sent by email during registration."
+        )
+        error_type_class = AccountError
+        error_type_field = "account_errors"
+
+    @classmethod
+    def perform_mutation(cls, _root, info: ResolveInfo, /, **data):
+        try:
+            user = models.User.objects.get(email=data["email"])
+        except ObjectDoesNotExist:
+            raise ValidationError(
+                {
+                    "email": ValidationError(
+                        "User with this email doesn't exist",
+                        code=AccountErrorCode.NOT_FOUND.value,
+                    )
+                }
+            )
+
+        if not default_token_generator.check_token(user, data["token"]):
+            raise ValidationError(
+                {
+                    "token": ValidationError(
+                        INVALID_TOKEN, code=AccountErrorCode.INVALID.value
+                    )
+                }
+            )
+
+        user.is_active = True
+        user.save(update_fields=["is_active", "updated_at"])
+
+        match_orders_with_new_user(user)
+        assign_user_gift_cards(user)
+
+        return ConfirmAccount(user=user)
+
+
+class PasswordChange(BaseMutation):
+    user = graphene.Field(User, description="A user instance with a new password.")
+
+    class Arguments:
+        old_password = graphene.String(
+            required=False, description="Current user password."
+        )
+        new_password = graphene.String(required=True, description="New user password.")
+
+    class Meta:
+        description = "Change the password of the logged in user."
+        error_type_class = AccountError
+        error_type_field = "account_errors"
+        permissions = (AuthorizationFilters.AUTHENTICATED_USER,)
+
+    @staticmethod
+    def raise_invalid_credentials():
+        raise ValidationError(
+            {
+                "old_password": ValidationError(
+                    "Old password isn't valid.",
+                    code=AccountErrorCode.INVALID_CREDENTIALS.value,
+                )
+            }
+        )
+
+    @classmethod
+    def perform_mutation(cls, _root, info: ResolveInfo, /, **data):
+        user = info.context.user
+        user = cast(models.User, user)
+        old_password = data.get("old_password")
+        new_password = data["new_password"]
+
+        if old_password is None:
+            # Spend time hashing useless password
+            # This prevents the outside actors from telling if user has
+            # unusable password set or not by measuring API's response time
+            make_password("waste-time")
+
+            if user.has_usable_password():
+                cls.raise_invalid_credentials()
+        elif not user.check_password(old_password):
+            cls.raise_invalid_credentials()
+        try:
+            password_validation.validate_password(new_password, user)
+        except ValidationError as error:
+            raise ValidationError({"new_password": error})
+
+        user.set_password(new_password)
+        user.save(update_fields=["password", "updated_at"])
+        account_events.customer_password_changed_event(user=user)
+        return PasswordChange(user=user)
+
+
+class BaseAddressUpdate(ModelMutation, I18nMixin):
+    """Base mutation for address update used by staff and account."""
+
+    user = graphene.Field(
+        User, description="A user object for which the address was edited."
+    )
+
+    class Arguments:
+        id = graphene.ID(description="ID of the address to update.", required=True)
+        input = AddressInput(
+            description="Fields required to update the address.", required=True
+        )
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def clean_input(cls, info: ResolveInfo, instance, data, **kwargs):
+        # Method check_permissions cannot be used for permission check, because
+        # it doesn't have the address instance.
+        check_can_edit_address(info.context, instance)
+        return super().clean_input(info, instance, data, **kwargs)
+
+    @classmethod
+    def perform_mutation(cls, _root, info: ResolveInfo, /, **data):
+        instance = cls.get_instance(info, **data)
+        cleaned_input = cls.clean_input(
+            info=info, instance=instance, data=data.get("input")
+        )
+        address = cls.validate_address(cleaned_input, instance=instance)
+        cls.clean_instance(info, address)
+        cls.save(info, address, cleaned_input)
+        cls._save_m2m(info, address, cleaned_input)
+
+        user = address.user_addresses.first()
+        if user:
+            user.search_document = prepare_user_search_document_value(user)
+            user.save(update_fields=["search_document", "updated_at"])
+        manager = get_plugin_manager_promise(info.context).get()
+        address = manager.change_user_address(address, None, user)
+        cls.call_event(manager.address_updated, address)
+
+        success_response = cls.success_response(address)
+        success_response.user = user
+        success_response.address = address
+        return success_respon
