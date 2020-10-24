93443295a9f5d26ce6decf1335ba6cc2ef55ec63
ZigBun <109135201+ZigBun@users.noreply.github.com>
2020-10-24 07:19:17 +0000

    update license file

--- saleor/graphql/account/mutations/base.py
+++ saleor/graphql/account/mutations/base.py
@@ -367,4 +367,207 @@ class BaseAddressUpdate(ModelMutation, I18nMixin):
         success_response = cls.success_response(address)
         success_response.user = user
         success_response.address = address
-        return success_respon
+        return success_response
+
+
+class BaseAddressDelete(ModelDeleteMutation):
+    """Base mutation for address delete used by staff and customers."""
+
+    user = graphene.Field(
+        User, description="A user instance for which the address was deleted."
+    )
+
+    class Arguments:
+        id = graphene.ID(required=True, description="ID of the address to delete.")
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def clean_instance(cls, info: ResolveInfo, instance) -> None:
+        # Method check_permissions cannot be used for permission check, because
+        # it doesn't have the address instance.
+        check_can_edit_address(info.context, instance)
+        return super().clean_instance(info, instance)
+
+    @classmethod
+    def perform_mutation(  # type: ignore[override]
+        cls, _root, info: ResolveInfo, /, *, id: str
+    ):
+        if not cls.check_permissions(info.context):
+            raise PermissionDenied()
+
+        instance = cls.get_node_or_error(info, id, only_type=Address)
+        if instance:
+            cls.clean_instance(info, instance)
+
+        db_id = instance.id
+
+        # Return the first user that the address is assigned to. There is M2M
+        # relation between users and addresses, but in most cases address is
+        # related to only one user.
+        user = instance.user_addresses.first()
+
+        instance.delete()
+        instance.id = db_id
+
+        if user:
+            # Refresh the user instance to clear the default addresses. If the
+            # deleted address was used as default, it would stay cached in the
+            # user instance and the invalid ID returned in the response might cause
+            # an error.
+            user.refresh_from_db()
+
+            user.search_document = prepare_user_search_document_value(user)
+            user.save(update_fields=["search_document", "updated_at"])
+
+        response = cls.success_response(instance)
+
+        response.user = user
+        manager = get_plugin_manager_promise(info.context).get()
+        cls.call_event(manager.address_deleted, instance)
+        return response
+
+
+class UserInput(graphene.InputObjectType):
+    first_name = graphene.String(description="Given name.")
+    last_name = graphene.String(description="Family name.")
+    email = graphene.String(description="The unique email address of the user.")
+    is_active = graphene.Boolean(required=False, description="User account is active.")
+    note = graphene.String(description="A note about the user.")
+
+
+class UserAddressInput(graphene.InputObjectType):
+    default_billing_address = AddressInput(
+        description="Billing address of the customer."
+    )
+    default_shipping_address = AddressInput(
+        description="Shipping address of the customer."
+    )
+
+
+class CustomerInput(UserInput, UserAddressInput):
+    language_code = graphene.Field(
+        LanguageCodeEnum, required=False, description="User language code."
+    )
+    external_reference = graphene.String(
+        description="External ID of the customer." + ADDED_IN_310, required=False
+    )
+
+
+class UserCreateInput(CustomerInput):
+    redirect_url = graphene.String(
+        description=(
+            "URL of a view where users should be redirected to "
+            "set the password. URL in RFC 1808 format."
+        )
+    )
+    channel = graphene.String(
+        description=(
+            "Slug of a channel which will be used for notify user. Optional when "
+            "only one channel exists."
+        )
+    )
+
+
+class BaseCustomerCreate(ModelMutation, I18nMixin):
+    """Base mutation for customer create used by staff and account."""
+
+    class Arguments:
+        input = UserCreateInput(
+            description="Fields required to create a customer.", required=True
+        )
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def clean_input(cls, info: ResolveInfo, instance, data, **kwargs):
+        shipping_address_data = data.pop(SHIPPING_ADDRESS_FIELD, None)
+        billing_address_data = data.pop(BILLING_ADDRESS_FIELD, None)
+        cleaned_input = super().clean_input(info, instance, data, **kwargs)
+
+        if shipping_address_data:
+            shipping_address = cls.validate_address(
+                shipping_address_data,
+                address_type=AddressType.SHIPPING,
+                instance=getattr(instance, SHIPPING_ADDRESS_FIELD),
+                info=info,
+            )
+            cleaned_input[SHIPPING_ADDRESS_FIELD] = shipping_address
+
+        if billing_address_data:
+            billing_address = cls.validate_address(
+                billing_address_data,
+                address_type=AddressType.BILLING,
+                instance=getattr(instance, BILLING_ADDRESS_FIELD),
+                info=info,
+            )
+            cleaned_input[BILLING_ADDRESS_FIELD] = billing_address
+
+        if cleaned_input.get("redirect_url"):
+            try:
+                validate_storefront_url(cleaned_input.get("redirect_url"))
+            except ValidationError as error:
+                raise ValidationError(
+                    {"redirect_url": error}, code=AccountErrorCode.INVALID.value
+                )
+
+        email = cleaned_input.get("email")
+        if email:
+            cleaned_input["email"] = email.lower()
+
+        return cleaned_input
+
+    @classmethod
+    @traced_atomic_transaction()
+    def save(cls, info: ResolveInfo, instance, cleaned_input):
+        default_shipping_address = cleaned_input.get(SHIPPING_ADDRESS_FIELD)
+        manager = get_plugin_manager_promise(info.context).get()
+        if default_shipping_address:
+            default_shipping_address = manager.change_user_address(
+                default_shipping_address, "shipping", instance
+            )
+            default_shipping_address.save()
+            instance.default_shipping_address = default_shipping_address
+        default_billing_address = cleaned_input.get(BILLING_ADDRESS_FIELD)
+        if default_billing_address:
+            default_billing_address = manager.change_user_address(
+                default_billing_address, "billing", instance
+            )
+            default_billing_address.save()
+            instance.default_billing_address = default_billing_address
+
+        is_creation = instance.pk is None
+        super().save(info, instance, cleaned_input)
+        if default_billing_address:
+            instance.addresses.add(default_billing_address)
+        if default_shipping_address:
+            instance.addresses.add(default_shipping_address)
+
+        instance.search_document = prepare_user_search_document_value(instance)
+        instance.save(update_fields=["search_document", "updated_at"])
+
+        # The instance is a new object in db, create an event
+        if is_creation:
+            manager.customer_created(customer=instance)
+            account_events.customer_account_created_event(user=instance)
+        else:
+            manager.customer_updated(instance)
+
+        if cleaned_input.get("redirect_url"):
+            channel_slug = cleaned_input.get("channel")
+            if not instance.is_staff:
+                channel_slug = clean_channel(
+                    channel_slug, error_class=AccountErrorCode
+                ).slug
+            elif channel_slug is not None:
+                channel_slug = validate_channel(
+                    channel_slug, error_class=AccountErrorCode
+                ).slug
+            send_set_password_notification(
+                cleaned_input.get("redirect_url"),
+                instance,
+                manager,
+                channel_slug,
+            )
