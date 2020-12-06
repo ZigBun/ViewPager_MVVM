52bb9e5c3d8d3eb8b014d1285a5a9c7b085c9093
ZigBun <109135201+ZigBun@users.noreply.github.com>
2020-12-06 16:22:10 +0000

    Fixed metadata

--- saleor/graphql/product/utils.py
+++ saleor/graphql/product/utils.py
@@ -51,4 +51,93 @@ def get_used_variants_attribute_values(product):
         ...
         {
             "attribute_1_global_id": ["ValueAttr1_2"],
-            "attribute_2_g
+            "attribute_2_global_id": ["ValueAttr2_2"]
+        }
+    ]
+    """
+    variants = (
+        product.variants.prefetch_related("attributes__values")
+        .prefetch_related("attributes__assignment")
+        .all()
+    )
+    used_attribute_values = []
+    for variant in variants:
+        attribute_values = get_used_attribute_values_for_variant(variant)
+        if attribute_values:
+            used_attribute_values.append(attribute_values)
+    return used_attribute_values
+
+
+@traced_atomic_transaction()
+def create_stocks(
+    variant: "ProductVariant",
+    stocks_data: List[Dict[str, str]],
+    warehouses: Iterable["Warehouse"],
+):
+    try:
+        new_stocks = Stock.objects.bulk_create(
+            [
+                Stock(
+                    product_variant=variant,
+                    warehouse=warehouse,
+                    quantity=stock_data["quantity"],
+                )
+                for stock_data, warehouse in zip(stocks_data, warehouses)
+            ]
+        )
+    except IntegrityError:
+        msg = "Stock for one of warehouses already exists for this product variant."
+        raise ValidationError(msg)
+    return new_stocks
+
+
+DraftOrderLinesData = namedtuple(
+    "DraftOrderLinesData", ["order_to_lines_mapping", "line_pks", "order_pks"]
+)
+
+
+def get_draft_order_lines_data_for_variants(
+    variant_ids: Iterable[int],
+):
+    lines = order_models.OrderLine.objects.filter(
+        variant__id__in=variant_ids, order__status=OrderStatus.DRAFT
+    ).select_related("order")
+    order_to_lines_mapping: Dict[
+        order_models.Order, List[order_models.OrderLine]
+    ] = defaultdict(list)
+    line_pks = set()
+    order_pks = set()
+    for line in lines:
+        order_to_lines_mapping[line.order].append(line)
+        line_pks.add(line.pk)
+        order_pks.add(line.order_id)
+
+    return DraftOrderLinesData(order_to_lines_mapping, line_pks, order_pks)
+
+
+def clean_variant_sku(sku: Optional[str]) -> Optional[str]:
+    if sku:
+        return sku.strip() or None
+    return None
+
+
+def update_ordered_media(ordered_media):
+    errors = defaultdict(list)
+    with transaction.atomic():
+        for order, media in enumerate(ordered_media):
+            media.sort_order = order
+            try:
+                media.save(update_fields=["sort_order"])
+            except DatabaseError as e:
+                msg = (
+                    f"Cannot update media for instance: {media}. "
+                    "Updating not existing object. "
+                    f"Details: {e}."
+                )
+                logger.warning(msg)
+                errors["media"].append(
+                    ValidationError(msg, code=ProductErrorCode.NOT_FOUND.value)
+                )
+
+    if errors:
+        raise ValidationError(errors)
