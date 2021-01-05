a9c5b6174e660ce14ff4f954b4846365aeb4d2b2
ZigBun <109135201+ZigBun@users.noreply.github.com>
2021-01-05 11:23:25 +0000

    msm: cpufreq: make the gov stop part fully atomic

---
+++ saleor/graphql/giftcard/dataloaders.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+from ...giftcard.models import GiftCard, GiftCardEvent, GiftCardTag
+from ...order.models import Order
+from ..core.dataloaders import DataLoader
+
+
+class GiftCardsByUserLoader(DataLoader):
+    context_key = "gift_cards_by_user"
+
+    def batch_load(self, keys):
+        gift_cards = GiftCard.objects.using(self.database_connection_name).filter(
+            used_by_id__in=keys
+        )
+        gift_cards_by_user_map = defaultdict(list)
+        for gift_card in gift_cards:
+            gift_cards_by_user_map[gift_card.used_by_id].append(gift_card)
+        return [gift_cards_by_user_map.get(user_id, []) for user_id in keys]
+
+
+class GiftCardEventsByGiftCardIdLoader(DataLoader):
+    context_key = "giftcardevents_by_giftcard"
+
+    def batch_load(self, keys):
+        events = GiftCardEvent.objects.using(self.database_connection_name).filter(
+            gift_card_id__in=keys
+        )
+        events_map = defaultdict(list)
+        for event in events.iterator():
+            events_map[event.gift_card_id].append(event)
+        return [events_ma
