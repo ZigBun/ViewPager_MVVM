94ce568a4277c1fa48bb895ea55b2997c430a0ab
ZigBun <109135201+ZigBun@users.noreply.github.com>
2021-01-30 12:28:13 +0000

    It compiles!

--- saleor/graphql/webhook/tests/queries/test_event_delivery_filter.py
+++ saleor/graphql/webhook/tests/queries/test_event_delivery_filter.py
@@ -48,4 +48,25 @@ def test_delivery_status_filter(
     # then
     assert delivery_response["edges"][0]["node"]["id"] == graphene.Node.to_global_id(
         "EventDelivery", event_delivery.pk
-    
+    )
+
+
+def test_delivery_status_filter_no_results(
+    event_delivery, staff_api_client, permission_manage_apps
+):
+    # given
+    webhook_id = graphene.Node.to_global_id("Webhook", event_delivery.webhook.pk)
+    staff_api_client.user.user_permissions.add(permission_manage_apps)
+    variables = {
+        "filters": {"status": EventDeliveryStatus.SUCCESS.upper()},
+        "id": webhook_id,
+        "first": 3,
+    }
+    # when
+    response = staff_api_client.post_graphql(
+        EVENT_DELIVERY_FILTER_QUERY, variables=variables
+    )
+    content = get_graphql_content(response)
+
+    # then
+    assert len(content["data"]["webhook"]["eventDeliveries"]["edges"]) == 0
