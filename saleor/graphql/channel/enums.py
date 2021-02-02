02849e25aadc99d7e309cc90fff087b63c54ad4c
ZigBun <109135201+ZigBun@users.noreply.github.com>
2021-02-02 21:41:04 +0000

    adapt rakefile to use yast rake config

---
+++ saleor/graphql/channel/enums.py
@@ -0,0 +1,9 @@
+
+from ...channel import AllocationStrategy
+from ..core.enums import to_enum
+
+AllocationStrategyEnum = to_enum(
+    AllocationStrategy,
+    type_name="AllocationStrategyEnum",
+    description=AllocationStrategy.__doc__,
+)
