7552bf602fb98baf7b6d4dfdb1dc414f64423aac
ZigBun <109135201+ZigBun@users.noreply.github.com>
2021-03-01 10:25:01 +0000

    Added quotes

--- saleor/order/migrations/0159_order_shipping_tax_class_and_more.py
+++ saleor/order/migrations/0159_order_shipping_tax_class_and_more.py
@@ -59,4 +59,41 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name="orderline",
-      
+            name="tax_class_metadata",
+            field=models.JSONField(
+                blank=True,
+                default=dict,
+                encoder=saleor.core.utils.json_serializer.CustomJsonEncoder,
+                null=True,
+            ),
+        ),
+        migrations.AddField(
+            model_name="orderline",
+            name="tax_class_name",
+            field=models.CharField(blank=True, max_length=255, null=True),
+        ),
+        migrations.AddField(
+            model_name="orderline",
+            name="tax_class_private_metadata",
+            field=models.JSONField(
+                blank=True,
+                default=dict,
+                encoder=saleor.core.utils.json_serializer.CustomJsonEncoder,
+                null=True,
+            ),
+        ),
+        migrations.AlterField(
+            model_name="order",
+            name="shipping_tax_rate",
+            field=models.DecimalField(
+                blank=True, decimal_places=4, max_digits=5, null=True
+            ),
+        ),
+        migrations.AlterField(
+            model_name="orderline",
+            name="tax_rate",
+            field=models.DecimalField(
+                blank=True, decimal_places=4, max_digits=5, null=True
+            ),
+        ),
+    ]
