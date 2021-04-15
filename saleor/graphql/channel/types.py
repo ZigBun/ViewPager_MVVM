7de927cc905ff6ab64eeb041b5bb0de71546687e
ZigBun <109135201+ZigBun@users.noreply.github.com>
2021-04-15 10:45:26 +0000

    ixgbe: Fix Tx Hang issue with lldpad on 82598EB

--- saleor/graphql/channel/types.py
+++ saleor/graphql/channel/types.py
@@ -73,4 +73,231 @@ class ChannelContextType(ChannelContextTypeForObjectType[T]):
 
     @classmethod
     def is_type_of(cls, root: Union[ChannelContext[T], T], _info: ResolveInfo) -> bool:
-        # Unwrap node 
+        # Unwrap node from ChannelContext if it didn't happen already
+        if isinstance(root, ChannelContext):
+            root = root.node
+
+        if isinstance(root, cls):
+            return True
+
+        if cls._meta.model._meta.proxy:
+            model = root._meta.model
+        else:
+            model = cast(Type[Model], root._meta.model._meta.concrete_model)
+
+        return model == cls._meta.model
+
+
+TM = TypeVar("TM", bound=ModelWithMetadata)
+
+
+class ChannelContextTypeWithMetadataForObjectType(ChannelContextTypeForObjectType[TM]):
+    """A Graphene type for that uses ChannelContext as root in resolvers.
+
+    Same as ChannelContextType, but for types that implement ObjectWithMetadata
+    interface.
+    """
+
+    class Meta:
+        abstract = True
+
+    @staticmethod
+    def resolve_metadata(root: ChannelContext[TM], info: ResolveInfo):
+        # Used in metadata API to resolve metadata fields from an instance.
+        return ObjectWithMetadata.resolve_metadata(root.node, info)
+
+    @staticmethod
+    def resolve_metafield(root: ChannelContext[TM], info: ResolveInfo, *, key: str):
+        # Used in metadata API to resolve metadata fields from an instance.
+        return ObjectWithMetadata.resolve_metafield(root.node, info, key=key)
+
+    @staticmethod
+    def resolve_metafields(root: ChannelContext[TM], info: ResolveInfo, *, keys=None):
+        # Used in metadata API to resolve metadata fields from an instance.
+        return ObjectWithMetadata.resolve_metafields(root.node, info, keys=keys)
+
+    @staticmethod
+    def resolve_private_metadata(root: ChannelContext[TM], info: ResolveInfo):
+        # Used in metadata API to resolve private metadata fields from an instance.
+        return ObjectWithMetadata.resolve_private_metadata(root.node, info)
+
+    @staticmethod
+    def resolve_private_metafield(
+        root: ChannelContext[TM], info: ResolveInfo, *, key: str
+    ):
+        # Used in metadata API to resolve private metadata fields from an instance.
+        return ObjectWithMetadata.resolve_private_metafield(root.node, info, key=key)
+
+    @staticmethod
+    def resolve_private_metafields(
+        root: ChannelContext[TM], info: ResolveInfo, *, keys=None
+    ):
+        # Used in metadata API to resolve private metadata fields from an instance.
+        return ObjectWithMetadata.resolve_private_metafields(root.node, info, keys=keys)
+
+
+class ChannelContextTypeWithMetadata(ChannelContextTypeWithMetadataForObjectType[TM]):
+    """A Graphene type for that uses ChannelContext as root in resolvers.
+
+    Same as ChannelContextType, but for types that implement ObjectWithMetadata
+    interface.
+    """
+
+    class Meta:
+        abstract = True
+
+
+class StockSettings(ObjectType):
+    allocation_strategy = AllocationStrategyEnum(
+        description=(
+            "Allocation strategy defines the preference of warehouses "
+            "for allocations and reservations."
+        ),
+        required=True,
+    )
+
+    class Meta:
+        description = (
+            "Represents the channel stock settings." + ADDED_IN_37 + PREVIEW_FEATURE
+        )
+
+
+class OrderSettings(ObjectType):
+    automatically_confirm_all_new_orders = graphene.Boolean(
+        required=True,
+        description=(
+            "When disabled, all new orders from checkout "
+            "will be marked as unconfirmed. When enabled orders from checkout will "
+            "become unfulfilled immediately."
+        ),
+    )
+    automatically_fulfill_non_shippable_gift_card = graphene.Boolean(
+        required=True,
+        description=(
+            "When enabled, all non-shippable gift card orders "
+            "will be fulfilled automatically."
+        ),
+    )
+
+    class Meta:
+        description = "Represents the channel-specific order settings."
+
+
+class Channel(ModelObjectType):
+    id = graphene.GlobalID(required=True)
+    slug = graphene.String(
+        required=True,
+        description="Slug of the channel.",
+    )
+
+    name = PermissionsField(
+        graphene.String,
+        description="Name of the channel.",
+        required=True,
+        permissions=[
+            AuthorizationFilters.AUTHENTICATED_APP,
+            AuthorizationFilters.AUTHENTICATED_STAFF_USER,
+        ],
+    )
+    is_active = PermissionsField(
+        graphene.Boolean,
+        description="Whether the channel is active.",
+        required=True,
+        permissions=[
+            AuthorizationFilters.AUTHENTICATED_APP,
+            AuthorizationFilters.AUTHENTICATED_STAFF_USER,
+        ],
+    )
+    currency_code = PermissionsField(
+        graphene.String,
+        description="A currency that is assigned to the channel.",
+        required=True,
+        permissions=[
+            AuthorizationFilters.AUTHENTICATED_APP,
+            AuthorizationFilters.AUTHENTICATED_STAFF_USER,
+        ],
+    )
+    has_orders = PermissionsField(
+        graphene.Boolean,
+        description="Whether a channel has associated orders.",
+        permissions=[
+            ChannelPermissions.MANAGE_CHANNELS,
+        ],
+        required=True,
+    )
+    default_country = PermissionsField(
+        CountryDisplay,
+        description=(
+            "Default country for the channel. Default country can be "
+            "used in checkout to determine the stock quantities or calculate taxes "
+            "when the country was not explicitly provided." + ADDED_IN_31
+        ),
+        required=True,
+        permissions=[
+            AuthorizationFilters.AUTHENTICATED_APP,
+            AuthorizationFilters.AUTHENTICATED_STAFF_USER,
+        ],
+    )
+    warehouses = PermissionsField(
+        NonNullList(Warehouse),
+        description="List of warehouses assigned to this channel."
+        + ADDED_IN_35
+        + PREVIEW_FEATURE,
+        required=True,
+        permissions=[
+            AuthorizationFilters.AUTHENTICATED_APP,
+            AuthorizationFilters.AUTHENTICATED_STAFF_USER,
+        ],
+    )
+    countries = NonNullList(
+        CountryDisplay,
+        description="List of shippable countries for the channel."
+        + ADDED_IN_36
+        + PREVIEW_FEATURE,
+    )
+
+    available_shipping_methods_per_country = graphene.Field(
+        NonNullList("saleor.graphql.shipping.types.ShippingMethodsPerCountry"),
+        countries=graphene.Argument(NonNullList(CountryCodeEnum)),
+        description="Shipping methods that are available for the channel."
+        + ADDED_IN_36
+        + PREVIEW_FEATURE,
+    )
+    stock_settings = PermissionsField(
+        StockSettings,
+        description=(
+            "Define the stock setting for this channel." + ADDED_IN_37 + PREVIEW_FEATURE
+        ),
+        required=True,
+        permissions=[
+            AuthorizationFilters.AUTHENTICATED_APP,
+            AuthorizationFilters.AUTHENTICATED_STAFF_USER,
+        ],
+    )
+    order_settings = PermissionsField(
+        OrderSettings,
+        description="Channel-specific order settings." + ADDED_IN_312,
+        required=True,
+        permissions=[
+            ChannelPermissions.MANAGE_CHANNELS,
+            OrderPermissions.MANAGE_ORDERS,
+        ],
+    )
+
+    class Meta:
+        description = "Represents channel."
+        model = models.Channel
+        interfaces = [graphene.relay.Node]
+
+    @staticmethod
+    def resolve_has_orders(root: models.Channel, info: ResolveInfo):
+        return (
+            ChannelWithHasOrdersByIdLoader(info.context)
+            .load(root.id)
+            .then(lambda channel: channel.has_orders)
+        )
+
+    @staticmethod
+    def resolve_default_country(root: models.Channel, _info: ResolveInfo):
+        return CountryDisplay(
+            code=root.default_country.code, country=ro
