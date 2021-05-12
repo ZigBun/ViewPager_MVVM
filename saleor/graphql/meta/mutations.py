a1605466a1e8d10c58dfa16e077b437d09dd4362
ZigBun <109135201+ZigBun@users.noreply.github.com>
2021-05-12 12:15:57 +0000

    Remove useless imports

--- saleor/graphql/meta/mutations.py
+++ saleor/graphql/meta/mutations.py
@@ -332,4 +332,66 @@ class UpdatePrivateMetadata(BaseMetadataMutation):
     class Meta:
         description = (
             "Updates private metadata of an object. To use it, you need to be an "
-            "authentica
+            "authenticated staff user or an app and have access to the modified object."
+        )
+        permission_map = PRIVATE_META_PERMISSION_MAP
+        error_type_class = MetadataError
+        error_type_field = "metadata_errors"
+
+    class Arguments:
+        id = graphene.ID(
+            description="ID or token (for Order and Checkout) of an object to update.",
+            required=True,
+        )
+        input = NonNullList(
+            MetadataInput,
+            description="Fields required to update the object's metadata.",
+            required=True,
+        )
+
+    @classmethod
+    def perform_mutation(cls, _root, info: ResolveInfo, /, **data):
+        instance = cls.get_instance(info, **data)
+        if instance:
+            meta_instance = get_valid_metadata_instance(instance)
+            metadata_list = data.pop("input")
+            cls.validate_metadata_keys(metadata_list)
+            items = {data.key: data.value for data in metadata_list}
+            meta_instance.store_value_in_private_metadata(items=items)
+            _save_instance(meta_instance, "private_metadata")
+        return cls.success_response(instance)
+
+
+class DeletePrivateMetadata(BaseMetadataMutation):
+    class Meta:
+        description = (
+            "Delete object's private metadata. To use it, you need to be an "
+            "authenticated staff user or an app and have access to the modified object."
+        )
+        permission_map = PRIVATE_META_PERMISSION_MAP
+        error_type_class = MetadataError
+        error_type_field = "metadata_errors"
+
+    class Arguments:
+        id = graphene.ID(
+            description="ID or token (for Order and Checkout) of an object to update.",
+            required=True,
+        )
+        keys = NonNullList(
+            graphene.String,
+            description="Metadata keys to delete.",
+            required=True,
+        )
+
+    @classmethod
+    def perform_mutation(  # type: ignore[override]
+        cls, _root, info: ResolveInfo, /, *, id: str, keys: List[str]
+    ):
+        instance = cls.get_instance(info, id=id)
+
+        if instance:
+            meta_instance = get_valid_metadata_instance(instance)
+            for key in keys:
+                meta_instance.delete_value_from_private_metadata(key)
+            _save_instance(meta_instance, "private_metadata")
+        return cls.success_response(instance)
