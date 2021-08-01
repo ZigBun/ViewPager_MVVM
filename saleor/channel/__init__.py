440d8fc7c7b4efe95f6c7ccdc5f10d7374fb71f7
ZigBun <109135201+ZigBun@users.noreply.github.com>
2021-08-01 13:52:53 +0000

    prod changes

---
+++ saleor/channel/__init__.py
@@ -0,0 +1,7 @@
+class AllocationStrategy:
+    """Determine the allocation strategy for the channel.
+
+    PRIORITIZE_SORTING_ORDER - allocate stocks according to the warehouses' order
+    within the channel
+
+    PRIORITIZE_HIGH_STOCK
