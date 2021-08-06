4eda5d5fc6027e96ed06aceeb609971b2b49e35d
ZigBun <109135201+ZigBun@users.noreply.github.com>
2021-08-06 22:19:33 +0000

    added breadcrumbs

--- saleor/webhook/observability/tests/test_payloads.py
+++ saleor/webhook/observability/tests/test_payloads.py
@@ -154,4 +154,216 @@ def test_serialize_gql_operation_results(gql_operation_factory):
     second_result = gql_operation_factory(query, "SecondQuery", None, result)
     payloads = serialize_gql_operation_results([first_result, second_result], 1024)
     assert payloads == [
-        G
+        GraphQLOperation(
+            name=JsonTruncText("FirstQuery", False),
+            operation_type="query",
+            query=JsonTruncText(query, False),
+            result=JsonTruncText(pretty_json(result), False),
+            result_invalid=False,
+        ),
+        GraphQLOperation(
+            name=JsonTruncText("SecondQuery", False),
+            operation_type="query",
+            query=JsonTruncText(query, False),
+            result=JsonTruncText(pretty_json(result), False),
+            result_invalid=False,
+        ),
+    ]
+
+
+def test_serialize_gql_operation_results_when_minimal_bytes_limit(
+    gql_operation_factory,
+):
+    query = "query FirstQuery { shop { name } } query SecondQuery { shop { name } }"
+    result = {"data": "result"}
+    first_result = gql_operation_factory(query, "FirstQuery", None, result)
+    second_result = gql_operation_factory(query, "SecondQuery", None, result)
+    payloads = serialize_gql_operation_results(
+        [first_result, second_result], 2 * GQL_OPERATION_PLACEHOLDER_SIZE
+    )
+    assert payloads == [
+        GraphQLOperation(
+            name=JsonTruncText("", True),
+            operation_type="query",
+            query=JsonTruncText("", True),
+            result=JsonTruncText("", True),
+            result_invalid=False,
+        ),
+        GraphQLOperation(
+            name=JsonTruncText("", True),
+            operation_type="query",
+            query=JsonTruncText("", True),
+            result=JsonTruncText("", True),
+            result_invalid=False,
+        ),
+    ]
+    assert len(dump_payload(payloads)) <= 2 * GQL_OPERATION_PLACEHOLDER_SIZE
+
+
+def test_serialize_gql_operation_results_when_too_low_bytes_limit(
+    gql_operation_factory,
+):
+    query = "query FirstQuery { shop { name } } query SecondQuery { shop { name } }"
+    result = {"data": "result"}
+    first_result = gql_operation_factory(query, "FirstQuery", None, result)
+    second_result = gql_operation_factory(query, "SecondQuery", None, result)
+    with pytest.raises(TruncationError):
+        serialize_gql_operation_results(
+            [first_result, second_result], 2 * GQL_OPERATION_PLACEHOLDER_SIZE - 1
+        )
+
+
+@pytest.mark.parametrize(
+    "headers,expected",
+    [
+        ({}, []),
+        (None, []),
+        (
+            {
+                "Authorization": "secret",
+                "Content-Length": "19",
+                "Content-Type": "application/json",
+            },
+            [
+                ("Authorization", MASK),
+                ("Content-Length", "19"),
+                ("Content-Type", "application/json"),
+            ],
+        ),
+    ],
+)
+def test_serialize_headers(headers, expected):
+    assert serialize_headers(headers) == expected
+
+
+def test_generate_api_call_payload(app, rf, gql_operation_factory, site_settings):
+    request = rf.post(
+        "/graphql", data={"request": "data"}, content_type="application/json"
+    )
+    request.request_time = datetime(1914, 6, 28, 10, 50, tzinfo=timezone.utc)
+    request.app = app
+    response = JsonResponse({"response": "data"})
+    query_a = "query FirstQuery { shop { name } }"
+    query_b = "query SecondQuery { shop { name } }"
+    result_a = {"data": "result A"}
+    result_b = {"data": "result B"}
+    first_result = gql_operation_factory(query_a, "FirstQuery", None, result_a)
+    second_result = gql_operation_factory(query_b, "SecondQuery", None, result_b)
+
+    payload = generate_api_call_payload(
+        request, response, [first_result, second_result], 2048
+    )
+    request_id = payload["request"]["id"]
+
+    assert UUID(request_id, version=4)
+    assert payload == ApiCallPayload(
+        event_type=ObservabilityEventTypes.API_CALL,
+        request=ApiCallRequest(
+            id=request_id,
+            method="POST",
+            url=f"http://{site_settings.site.domain}/graphql",
+            time=request.request_time.timestamp(),
+            content_length=19,
+            headers=[
+                ("Cookie", "***"),
+                ("Content-Length", "19"),
+                ("Content-Type", "application/json"),
+            ],
+        ),
+        app=App(
+            id=graphene.Node.to_global_id("App", app.pk), name="Sample app objects"
+        ),
+        response=ApiCallResponse(
+            headers=[
+                ("Content-Type", "application/json"),
+            ],
+            status_code=200,
+            content_length=20,
+        ),
+        gql_operations=[
+            GraphQLOperation(
+                name=JsonTruncText("FirstQuery", False),
+                operation_type="query",
+                query=JsonTruncText(query_a, False),
+                result=JsonTruncText(pretty_json(result_a), False),
+                result_invalid=False,
+            ),
+            GraphQLOperation(
+                name=JsonTruncText("SecondQuery", False),
+                operation_type="query",
+                query=JsonTruncText(query_b, False),
+                result=JsonTruncText(pretty_json(result_b), False),
+                result_invalid=False,
+            ),
+        ],
+    )
+
+
+def test_generate_api_call_payload_request_not_from_app(rf):
+    request = rf.post(
+        "/graphql", data={"request": "data"}, content_type="application/json"
+    )
+    request.app = None
+    response = JsonResponse({"response": "data"})
+    payload = generate_api_call_payload(request, response, [], 1024)
+
+    assert payload["app"] is None
+
+
+def test_generate_api_call_payload_skip_operations_when_size_limit_too_low(
+    app, rf, gql_operation_factory
+):
+    request = rf.post(
+        "/graphql", data={"request": "data"}, content_type="application/json"
+    )
+    request.request_time = datetime(1914, 6, 28, 10, 50, tzinfo=timezone.utc)
+    request.app = app
+    response = JsonResponse({"response": "data"})
+    query = "query FirstQuery { shop { name } } query SecondQuery { shop { name } }"
+    result = {"data": "result A"}
+    first_result = gql_operation_factory(query, "FirstQuery", None, result)
+    second_result = gql_operation_factory(query, "SecondQuery", None, result)
+    payload_without_operations = generate_api_call_payload(request, response, [], 1024)
+    bytes_limit = (
+        len(dump_payload(payload_without_operations))
+        + GQL_OPERATION_PLACEHOLDER_SIZE * 2
+    )
+    operation_trunc_payload = GraphQLOperation(
+        name=JsonTruncText("", True),
+        operation_type="query",
+        query=JsonTruncText("", True),
+        result=JsonTruncText("", True),
+        result_invalid=False,
+    )
+
+    payload = generate_api_call_payload(
+        request, response, [first_result, second_result], bytes_limit
+    )
+
+    assert payload["gql_operations"] == [operation_trunc_payload] * 2
+
+
+def test_generate_api_call_payload_when_too_low_bytes_limit(app, rf):
+    request = rf.post(
+        "/graphql", data={"request": "data"}, content_type="application/json"
+    )
+    request.request_time = datetime(1914, 6, 28, 10, 50, tzinfo=timezone.utc)
+    request.app = app
+    response = JsonResponse({"response": "data"})
+    payload = generate_api_call_payload(request, response, [], 1024)
+
+    with pytest.raises(TruncationError):
+        generate_api_call_payload(request, response, [], len(payload) - 1)
+
+
+def test_generate_event_delivery_attempt_payload(event_attempt):
+    created_at = datetime(1914, 6, 28, 10, 50, tzinfo=timezone.utc)
+    event_attempt.created_at = created_at
+    delivery = event_attempt.delivery
+    webhook = delivery.webhook
+    app = webhook.app
+
+    payload = generate_event_delivery_attempt_payload(event_attempt, None, 1024)
+
+    assert payload == EventDeliveryAttemptPayload(
+        id=graphen
