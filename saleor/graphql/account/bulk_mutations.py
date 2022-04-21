6947e528ab371769d98f3b75ddbdb8377b7db809
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-04-21 10:51:55 +0000

    Favicon

--- saleor/graphql/account/bulk_mutations.py
+++ saleor/graphql/account/bulk_mutations.py
@@ -62,4 +62,80 @@ class StaffBulkDelete(StaffDeleteMixin, UserBulkDelete):
 
     @classmethod
     def perform_mutation(  # type: ignore[override]
-      
+        cls, _root, info: ResolveInfo, /, *, ids, **data
+    ):
+        instances = cls.get_nodes_or_error(ids, "id", User)
+        errors = cls.clean_instances(info, instances)
+        count = len(instances)
+        if not errors and count:
+            clean_instance_ids = [instance.pk for instance in instances]
+            qs = models.User.objects.filter(pk__in=clean_instance_ids)
+            cls.bulk_action(info, qs, **data)
+        else:
+            count = 0
+        return count, errors
+
+    @classmethod
+    def clean_instances(cls, info: ResolveInfo, users):
+        errors: defaultdict[str, List[ValidationError]] = defaultdict(list)
+
+        requestor = info.context.user
+        cls.check_if_users_can_be_deleted(info, users, "ids", errors)
+        cls.check_if_requestor_can_manage_users(requestor, users, "ids", errors)
+        cls.check_if_removing_left_not_manageable_permissions(
+            requestor, users, "ids", errors
+        )
+        return ValidationError(errors) if errors else {}
+
+    @classmethod
+    def bulk_action(cls, info: ResolveInfo, queryset, /):
+        instances = list(queryset)
+        queryset.delete()
+        manager = get_plugin_manager_promise(info.context).get()
+        for instance in instances:
+            manager.staff_deleted(instance)
+
+
+class UserBulkSetActive(BaseBulkMutation):
+    class Arguments:
+        ids = NonNullList(
+            graphene.ID, required=True, description="List of user IDs to (de)activate)."
+        )
+        is_active = graphene.Boolean(
+            required=True, description="Determine if users will be set active or not."
+        )
+
+    class Meta:
+        description = "Activate or deactivate users."
+        model = models.User
+        object_type = User
+        permissions = (AccountPermissions.MANAGE_USERS,)
+        error_type_class = AccountError
+        error_type_field = "account_errors"
+
+    @classmethod
+    def clean_instance(cls, info: ResolveInfo, instance):
+        if info.context.user == instance:
+            raise ValidationError(
+                {
+                    "is_active": ValidationError(
+                        "Cannot activate or deactivate your own account.",
+                        code=AccountErrorCode.ACTIVATE_OWN_ACCOUNT.value,
+                    )
+                }
+            )
+        elif instance.is_superuser:
+            raise ValidationError(
+                {
+                    "is_active": ValidationError(
+                        "Cannot activate or deactivate superuser's account.",
+                        code=AccountErrorCode.ACTIVATE_SUPERUSER_ACCOUNT.value,
+                    )
+                }
+            )
+
+    @classmethod
+    def bulk_action(  # type: ignore[override]
+        cls, _info: ResolveInfo, queryset, /, *, is_active
+    ):
+        queryset.update(is_active=is_active)
