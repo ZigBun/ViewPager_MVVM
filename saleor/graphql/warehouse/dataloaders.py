68e1df4dcf3e04f4f773517e048e94e4e41b8c01
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-04-22 10:30:09 +0000

    thumbnails

--- saleor/graphql/warehouse/dataloaders.py
+++ saleor/graphql/warehouse/dataloaders.py
@@ -185,4 +185,165 @@ class AvailableQuantityByProductVariantIdCountryCodeAndChannelSlugLoader(
                     .filter(slug=channel_slug)
                     .values("pk")
                 )
-                shipping_zone_channels =
+                shipping_zone_channels = (
+                    ShippingZoneChannel.objects.using(self.database_connection_name)
+                    .filter(Exists(channels.filter(pk=OuterRef("channel_id"))))
+                    .values("shippingzone_id")
+                )
+                warehouse_channels = (
+                    WarehouseChannel.objects.using(self.database_connection_name)
+                    .filter(
+                        Exists(channels.filter(pk=OuterRef("channel_id"))),
+                    )
+                    .values("warehouse_id")
+                )
+                warehouse_shipping_zones = warehouse_shipping_zones.filter(
+                    Exists(
+                        shipping_zone_channels.filter(
+                            shippingzone_id=OuterRef("shippingzone_id")
+                        )
+                    ),
+                    Exists(
+                        warehouse_channels.filter(warehouse_id=OuterRef("warehouse_id"))
+                    ),
+                )
+        return warehouse_shipping_zones
+
+    def get_click_and_collect_warehouses(self, channel_slug, country_code):
+        """Get the collection point warehouses for a given channel and country code."""
+        warehouses = Warehouse.objects.none()
+        if not country_code and channel_slug:
+            channels = (
+                Channel.objects.using(self.database_connection_name)
+                .filter(slug=channel_slug)
+                .values("pk")
+            )
+            WarehouseChannel = Channel.warehouses.through  # type: ignore[attr-defined] # raw access to the through model # noqa: E501
+            warehouse_channels = (
+                WarehouseChannel.objects.using(self.database_connection_name)
+                .filter(
+                    Exists(channels.filter(pk=OuterRef("channel_id"))),
+                )
+                .values("warehouse_id")
+            )
+            warehouses = Warehouse.objects.filter(
+                Exists(warehouse_channels.filter(warehouse_id=OuterRef("id"))),
+                click_and_collect_option__in=[
+                    WarehouseClickAndCollectOption.LOCAL_STOCK,
+                    WarehouseClickAndCollectOption.ALL_WAREHOUSES,
+                ],
+            )
+        return warehouses
+
+    def prepare_stocks_reservations_map(self, variant_ids):
+        """Prepare stock id to quantity reserved map for provided variant ids."""
+        stocks_reservations = defaultdict(int)
+        site = get_site_promise(self.context).get()
+        if is_reservation_enabled(site.settings):
+            # Can't do second annotation on same queryset because it made
+            # available_quantity annotated value incorrect thanks to how
+            # Django's ORM builds SQLs with annotations
+            reservations_qs = (
+                Stock.objects.using(self.database_connection_name)
+                .filter(product_variant_id__in=variant_ids)
+                .annotate_reserved_quantity()
+                .values_list("id", "reserved_quantity")
+            )
+            for stock_id, quantity_reserved in reservations_qs:
+                stocks_reservations[stock_id] = quantity_reserved
+        return stocks_reservations
+
+    def prepare_warehouse_ids_by_shipping_zone_and_variant_map(
+        self,
+        stocks: QuerySet[StockWithAvailableQuantity],
+        stocks_reservations,
+        warehouse_shipping_zones_map,
+        cc_warehouses,
+    ):
+        """Combine all quantities within a single zone.
+
+        Prepare `warehouse_ids_by_shipping_zone_by_variant` map in the following format:
+            {
+                variant_id: {
+                    shipping_zone_id/warehouse_id: [
+                        warehouse_id
+                    ]
+                }
+            }
+
+        In case of the collection point warehouses the warehouse_id is used instead of
+        the shipping zone id. Every stock of the collection point warehouse is treated
+        as a magic single-warehouse shipping zone.
+        """
+        cc_warehouses_in_bulk = cc_warehouses.in_bulk()
+        warehouse_ids_by_shipping_zone_by_variant: DefaultDict[
+            int, DefaultDict[Union[int, UUID], List[UUID]]
+        ] = defaultdict(lambda: defaultdict(list))
+        variants_with_global_cc_warehouses = []
+        available_quantity_by_warehouse_id_and_variant_id: DefaultDict[
+            UUID, Dict[int, int]
+        ] = defaultdict(lambda: defaultdict(int))
+        for stock in stocks:
+            reserved_quantity = stocks_reservations[stock.id]
+            quantity = stock.available_quantity - reserved_quantity
+            # when the available_quantity was under 0 we do not want clipping to zero,
+            # as it means that the stock might be exceeded
+            if stock.available_quantity > 0:
+                quantity = max(0, quantity)
+            variant_id = stock.product_variant_id
+            warehouse_id = stock.warehouse_id
+            available_quantity_by_warehouse_id_and_variant_id[warehouse_id][
+                variant_id
+            ] += quantity
+            if shipping_zone_ids := warehouse_shipping_zones_map[warehouse_id]:
+                for shipping_zone_id in shipping_zone_ids:
+                    warehouse_ids_by_shipping_zone_by_variant[variant_id][
+                        shipping_zone_id
+                    ].append(warehouse_id)
+            else:
+                cc_option = cc_warehouses_in_bulk[warehouse_id].click_and_collect_option
+                # every stock of a collection point warehouse should treat as a magic
+                # single-warehouse shipping zone
+                warehouse_ids_by_shipping_zone_by_variant[variant_id][warehouse_id] = [
+                    warehouse_id
+                ]
+                # in case of global warehouses the quantity available will be the sum
+                # of the available quantity for that variant from all stocks,
+                # so we need to keep information for which variant there is a warehouse
+                # with the global stock
+                if cc_option == WarehouseClickAndCollectOption.ALL_WAREHOUSES:
+                    variants_with_global_cc_warehouses.append(variant_id)
+        return (
+            warehouse_ids_by_shipping_zone_by_variant,
+            variants_with_global_cc_warehouses,
+            available_quantity_by_warehouse_id_and_variant_id,
+        )
+
+    def prepare_quantity_map(
+        self,
+        country_code,
+        warehouse_ids_by_shipping_zone_by_variant,
+        variants_with_global_cc_warehouses,
+        available_quantity_by_warehouse_id_and_variant_id,
+    ):
+        """Prepare the variant id to quantity map.
+
+        When the country code is known, the available quantity is the sum of quantities
+        from all shipping zones supporting given country. When the country is not known
+        the highest known quantity is returned.
+
+        The local warehouses are treated as a magic single-warehouse shipping zone.
+        When the variant has any global collection point warehouse, the quantity is the
+        sum of the quantities from all shipping zones.
+        In case of global warehouses the available quantity of such collection point
+        is the sum of the available quantities from all stocks that passed the country
+        or channel conditions.
+        """
+        quantity_map: DefaultDict[int, int] = defaultdict(int)
+        for (
+            variant_id,
+            warehouse_ids_shipping_zone,
+        ) in warehouse_ids_by_shipping_zone_by_variant.items():
+            if country_code or variant_id in variants_with_global_cc_warehouses:
+                used_warehouse_ids = []
+                for warehouse_ids in warehouse_ids_sh
