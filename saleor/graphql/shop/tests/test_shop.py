a6a4521f5a94d2d0db39fc8d2cc02d77bf8e4427
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-05-28 22:46:35 +0000

    v2.0-rc.1

--- saleor/graphql/shop/tests/test_shop.py
+++ saleor/graphql/shop/tests/test_shop.py
@@ -1219,4 +1219,233 @@ MUTATION_STAFF_NOTIFICATION_RECIPIENT_UPDATE = """
         $id: ID!,
         $input: StaffNotificationRecipientInput!
     ) {
-  
+        staffNotificationRecipientUpdate(id: $id, input: $input) {
+            staffNotificationRecipient {
+                active
+                email
+                user {
+                    firstName
+                    lastName
+                    email
+                }
+            }
+            errors {
+                field
+                message
+                code
+            }
+        }
+    }
+"""
+
+
+def test_staff_notification_update_mutation(
+    staff_api_client,
+    staff_user,
+    permission_manage_settings,
+    staff_notification_recipient,
+):
+    old_email = staff_notification_recipient.get_email()
+    assert staff_notification_recipient.active
+    staff_notification_recipient_id = graphene.Node.to_global_id(
+        "StaffNotificationRecipient", staff_notification_recipient.id
+    )
+    variables = {"id": staff_notification_recipient_id, "input": {"active": False}}
+    staff_api_client.post_graphql(
+        MUTATION_STAFF_NOTIFICATION_RECIPIENT_UPDATE,
+        variables,
+        permissions=[permission_manage_settings],
+    )
+    staff_notification_recipient.refresh_from_db()
+    assert not staff_notification_recipient.active
+    assert staff_notification_recipient.get_email() == old_email
+
+
+def test_staff_notification_update_mutation_with_empty_user(
+    staff_api_client,
+    staff_user,
+    permission_manage_settings,
+    staff_notification_recipient,
+):
+    staff_notification_recipient_id = graphene.Node.to_global_id(
+        "StaffNotificationRecipient", staff_notification_recipient.id
+    )
+    variables = {"id": staff_notification_recipient_id, "input": {"user": ""}}
+    response = staff_api_client.post_graphql(
+        MUTATION_STAFF_NOTIFICATION_RECIPIENT_UPDATE,
+        variables,
+        permissions=[permission_manage_settings],
+    )
+    content = get_graphql_content(response)
+
+    staff_notification_recipient.refresh_from_db()
+    assert content["data"]["staffNotificationRecipientUpdate"] == {
+        "staffNotificationRecipient": None,
+        "errors": [
+            {
+                "code": "INVALID",
+                "field": "staffNotification",
+                "message": "User and email cannot be set empty",
+            }
+        ],
+    }
+
+
+def test_staff_notification_update_mutation_with_empty_email(
+    staff_api_client,
+    staff_user,
+    permission_manage_settings,
+    staff_notification_recipient,
+):
+    staff_notification_recipient_id = graphene.Node.to_global_id(
+        "StaffNotificationRecipient", staff_notification_recipient.id
+    )
+    variables = {"id": staff_notification_recipient_id, "input": {"email": ""}}
+    response = staff_api_client.post_graphql(
+        MUTATION_STAFF_NOTIFICATION_RECIPIENT_UPDATE,
+        variables,
+        permissions=[permission_manage_settings],
+    )
+    content = get_graphql_content(response)
+
+    staff_notification_recipient.refresh_from_db()
+    assert content["data"]["staffNotificationRecipientUpdate"] == {
+        "staffNotificationRecipient": None,
+        "errors": [
+            {
+                "code": "INVALID",
+                "field": "staffNotification",
+                "message": "User and email cannot be set empty",
+            }
+        ],
+    }
+
+
+ORDER_SETTINGS_UPDATE_MUTATION = """
+    mutation orderSettings($confirmOrders: Boolean, $fulfillGiftCards: Boolean) {
+        orderSettingsUpdate(
+            input: {
+                automaticallyConfirmAllNewOrders: $confirmOrders
+                automaticallyFulfillNonShippableGiftCard: $fulfillGiftCards
+            }
+        ) {
+            orderSettings {
+                automaticallyConfirmAllNewOrders
+                automaticallyFulfillNonShippableGiftCard
+            }
+        }
+    }
+"""
+
+
+def test_order_settings_update_by_staff(
+    staff_api_client, permission_manage_orders, channel_USD, channel_PLN
+):
+    # given
+    staff_api_client.user.user_permissions.add(permission_manage_orders)
+
+    # when
+    response = staff_api_client.post_graphql(
+        ORDER_SETTINGS_UPDATE_MUTATION,
+        {"confirmOrders": False, "fulfillGiftCards": False},
+    )
+    content = get_graphql_content(response)
+
+    # then
+    response_settings = content["data"]["orderSettingsUpdate"]["orderSettings"]
+    assert response_settings["automaticallyConfirmAllNewOrders"] is False
+    assert response_settings["automaticallyFulfillNonShippableGiftCard"] is False
+    channel_PLN.refresh_from_db()
+    channel_USD.refresh_from_db()
+    assert channel_PLN.automatically_confirm_all_new_orders is False
+    assert channel_PLN.automatically_fulfill_non_shippable_gift_card is False
+    assert channel_USD.automatically_confirm_all_new_orders is False
+    assert channel_USD.automatically_fulfill_non_shippable_gift_card is False
+
+
+def test_order_settings_update_by_staff_nothing_changed(
+    staff_api_client, permission_manage_orders, channel_USD, channel_PLN
+):
+    # given
+    staff_api_client.user.user_permissions.add(permission_manage_orders)
+    QUERY = """
+        mutation {
+            orderSettingsUpdate(
+                input: {}
+            ) {
+                orderSettings {
+                    automaticallyConfirmAllNewOrders
+                    automaticallyFulfillNonShippableGiftCard
+                }
+            }
+        }
+    """
+
+    # when
+    response = staff_api_client.post_graphql(
+        QUERY,
+        {},
+    )
+    content = get_graphql_content(response)
+
+    # then
+    response_settings = content["data"]["orderSettingsUpdate"]["orderSettings"]
+    assert response_settings["automaticallyConfirmAllNewOrders"] is True
+    assert response_settings["automaticallyFulfillNonShippableGiftCard"] is True
+    channel_PLN.refresh_from_db()
+    channel_USD.refresh_from_db()
+    assert channel_PLN.automatically_confirm_all_new_orders is True
+    assert channel_PLN.automatically_fulfill_non_shippable_gift_card is True
+    assert channel_USD.automatically_confirm_all_new_orders is True
+    assert channel_USD.automatically_fulfill_non_shippable_gift_card is True
+
+
+def test_order_settings_update_by_app(
+    app_api_client, permission_manage_orders, channel_USD, channel_PLN
+):
+    # given
+    app_api_client.app.permissions.set([permission_manage_orders])
+
+    # when
+    response = app_api_client.post_graphql(
+        ORDER_SETTINGS_UPDATE_MUTATION,
+        {"confirmOrders": False, "fulfillGiftCards": False},
+    )
+    content = get_graphql_content(response)
+
+    # then
+    response_settings = content["data"]["orderSettingsUpdate"]["orderSettings"]
+    assert response_settings["automaticallyConfirmAllNewOrders"] is False
+    assert response_settings["automaticallyFulfillNonShippableGiftCard"] is False
+    channel_PLN.refresh_from_db()
+    channel_USD.refresh_from_db()
+    assert channel_PLN.automatically_confirm_all_new_orders is False
+    assert channel_PLN.automatically_fulfill_non_shippable_gift_card is False
+    assert channel_USD.automatically_confirm_all_new_orders is False
+    assert channel_USD.automatically_fulfill_non_shippable_gift_card is False
+
+
+def test_order_settings_update_by_user_without_permissions(
+    user_api_client, channel_USD, channel_PLN
+):
+    # given
+
+    # when
+    response = user_api_client.post_graphql(
+        ORDER_SETTINGS_UPDATE_MUTATION,
+        {"confirmOrders": False, "fulfillGiftCards": False},
+    )
+
+    # then
+    assert_no_permission(response)
+    channel_PLN.refresh_from_db()
+    channel_USD.refresh_from_db()
+    assert channel_PLN.automatically_confirm_all_new_orders is True
+    assert channel_PLN.automatically_fulfill_non_shippable_gift_card is True
+    assert channel_USD.automatically_confirm_all_new_orders is True
+    assert channel_USD.automatically_fulfill_non_shippable_gift_card is True
+
+
+ORDER_SETTINGS_QUERY = """
+    query orderSettings {
+        ord
