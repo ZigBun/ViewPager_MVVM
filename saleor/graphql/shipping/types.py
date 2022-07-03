f6231e29a90518976ae5fd02fc10b15d2d3cd8be
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-07-03 12:02:31 +0000

    v6.0.2

--- saleor/graphql/shipping/types.py
+++ saleor/graphql/shipping/types.py
@@ -109,4 +109,214 @@ class ShippingMethodType(ChannelContextTypeWithMetadataForObjectType):
     translation = TranslationField(
         ShippingMethodTranslation,
         type_name="shipping method",
-        resolver=None,  # Disable defa
+        resolver=None,  # Disable default resolver
+    )
+    channel_listings = PermissionsField(
+        NonNullList(ShippingMethodChannelListing),
+        description="List of channels available for the method.",
+        permissions=[
+            ShippingPermissions.MANAGE_SHIPPING,
+        ],
+    )
+    maximum_order_price = graphene.Field(
+        Money, description="The price of the cheapest variant (including discounts)."
+    )
+    minimum_order_price = graphene.Field(
+        Money, description="The price of the cheapest variant (including discounts)."
+    )
+    postal_code_rules = NonNullList(
+        ShippingMethodPostalCodeRule,
+        description=(
+            "Postal code ranges rule of exclusion or inclusion of the shipping method."
+        ),
+    )
+    excluded_products = ConnectionField(
+        "saleor.graphql.product.types.products.ProductCountableConnection",
+        description="List of excluded products for the shipping method.",
+        permissions=[ShippingPermissions.MANAGE_SHIPPING],
+    )
+    minimum_order_weight = graphene.Field(
+        Weight, description="Minimum order weight to use this shipping method."
+    )
+    maximum_order_weight = graphene.Field(
+        Weight, description="Maximum order weight to use this shipping method."
+    )
+    maximum_delivery_days = graphene.Int(
+        description="Maximum number of days for delivery."
+    )
+    minimum_delivery_days = graphene.Int(
+        description="Minimal number of days for delivery."
+    )
+    tax_class = PermissionsField(
+        TaxClass,
+        description="Tax class assigned to this shipping method.",
+        required=False,
+        permissions=[
+            CheckoutPermissions.MANAGE_TAXES,
+            ShippingPermissions.MANAGE_SHIPPING,
+        ],
+    )
+
+    class Meta:
+        default_resolver = ChannelContextType.resolver_with_context
+        description = (
+            "Shipping method are the methods you'll use to get customer's orders to "
+            "them. They are directly exposed to the customers."
+        )
+        interfaces = [relay.Node, ObjectWithMetadata]
+        model = models.ShippingMethod
+
+    @staticmethod
+    def resolve_id(root: ChannelContext, _info):
+        return graphene.Node.to_global_id("ShippingMethodType", root.node.id)
+
+    @staticmethod
+    def resolve_maximum_order_price(root: ChannelContext[models.ShippingMethod], info):
+        maximum_order_price = getattr(root.node, "maximum_order_price", None)
+        if maximum_order_price is not None:
+            return maximum_order_price
+
+        if not root.channel_slug:
+            return None
+
+        return (
+            ShippingMethodChannelListingByShippingMethodIdAndChannelSlugLoader(
+                info.context
+            )
+            .load((root.node.id, root.channel_slug))
+            .then(lambda channel_listing: channel_listing.maximum_order_price)
+        )
+
+    @staticmethod
+    def resolve_minimum_order_price(root: ChannelContext[models.ShippingMethod], info):
+        minimum_order_price = getattr(root.node, "minimum_order_price", None)
+        if minimum_order_price is not None:
+            return minimum_order_price
+
+        if not root.channel_slug:
+            return None
+
+        return (
+            ShippingMethodChannelListingByShippingMethodIdAndChannelSlugLoader(
+                info.context
+            )
+            .load((root.node.id, root.channel_slug))
+            .then(lambda channel_listing: channel_listing.minimum_order_price)
+        )
+
+    @staticmethod
+    def resolve_maximum_order_weight(
+        root: ChannelContext[models.ShippingMethod], _info
+    ):
+        return convert_weight_to_default_weight_unit(root.node.maximum_order_weight)
+
+    @staticmethod
+    def resolve_postal_code_rules(root: ChannelContext[models.ShippingMethod], info):
+        return PostalCodeRulesByShippingMethodIdLoader(info.context).load(root.node.id)
+
+    @staticmethod
+    def resolve_minimum_order_weight(
+        root: ChannelContext[models.ShippingMethod], _info
+    ):
+        return convert_weight_to_default_weight_unit(root.node.minimum_order_weight)
+
+    @staticmethod
+    def resolve_channel_listings(root: ChannelContext[models.ShippingMethod], info):
+        return ShippingMethodChannelListingByShippingMethodIdLoader(info.context).load(
+            root.node.id
+        )
+
+    @staticmethod
+    def resolve_excluded_products(
+        root: ChannelContext[models.ShippingMethod], info, **kwargs
+    ):
+        from ..product.types import ProductCountableConnection
+
+        qs: Union[QuerySet[product_models.Product], ChannelQsContext]
+
+        if not root.node.excluded_products:
+            qs = product_models.Product.objects.none()
+        else:
+            qs = ChannelQsContext(
+                qs=root.node.excluded_products.all(), channel_slug=None
+            )
+
+        return create_connection_slice(qs, info, kwargs, ProductCountableConnection)
+
+    @staticmethod
+    def resolve_tax_class(root: ChannelContext[models.ShippingMethod], info):
+        return (
+            TaxClassByIdLoader(info.context).load(root.node.tax_class_id)
+            if root.node.tax_class_id
+            else None
+        )
+
+
+class ShippingZone(ChannelContextTypeWithMetadata[models.ShippingZone]):
+    id = graphene.GlobalID(required=True)
+    name = graphene.String(required=True)
+    default = graphene.Boolean(required=True)
+    price_range = graphene.Field(
+        MoneyRange, description="Lowest and highest prices for the shipping."
+    )
+    countries = NonNullList(
+        CountryDisplay,
+        description="List of countries available for the method.",
+        required=True,
+    )
+    shipping_methods = NonNullList(
+        ShippingMethodType,
+        description=(
+            "List of shipping methods available for orders"
+            " shipped to countries within this shipping zone."
+        ),
+    )
+    warehouses = NonNullList(
+        Warehouse,
+        description="List of warehouses for shipping zone.",
+        required=True,
+    )
+    channels = NonNullList(
+        Channel,
+        description="List of channels for shipping zone.",
+        required=True,
+    )
+    description = graphene.String(description="Description of a shipping zone.")
+
+    class Meta:
+        default_resolver = ChannelContextType.resolver_with_context
+        description = (
+            "Represents a shipping zone in the shop. Zones are the concept used only "
+            "for grouping shipping methods in the dashboard, and are never exposed to "
+            "the customers directly."
+        )
+        model = models.ShippingZone
+        interfaces = [relay.Node, ObjectWithMetadata]
+
+    @staticmethod
+    @traced_resolver
+    def resolve_price_range(root: ChannelContext[models.ShippingZone], _info):
+        return resolve_price_range(root.channel_slug)
+
+    @staticmethod
+    def resolve_countries(root: ChannelContext[models.ShippingZone], _info):
+        return [
+            CountryDisplay(code=country.code, country=country.name)
+            for country in root.node.countries
+        ]
+
+    @staticmethod
+    def resolve_shipping_methods(root: ChannelContext[models.ShippingZone], info):
+        def wrap_shipping_method_with_channel_context(shipping_methods):
+            shipping_methods = [
+                ChannelContext(node=shipping, channel_slug=root.channel_slug)
+                for shipping in shipping_methods
+            ]
+            return shipping_methods
+
+        channel_slug = root.channel_slug
+        if channel_slug:
+            return (
+                ShippingMethodsByShippingZoneIdAndChannelSlugLoader(info.context)
+                .load((root.node.id, channel_slug))
+      
