2704522d4e924e4a68ce1f226130226bb510b71b
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-07-20 14:41:35 +0000

    add github link

--- saleor/order/utils.py
+++ saleor/order/utils.py
@@ -354,4 +354,246 @@ def add_variant_to_order(
 
         if allocate_stock:
             increase_allocations(
-           
+                [
+                    OrderLineInfo(
+                        line=line,
+                        quantity=line_data.quantity,
+                        variant=line_data.variant,
+                        warehouse_pk=None,
+                    )
+                ],
+                channel,
+                manager=manager,
+            )
+
+        return line
+
+    if line_data.variant_id:
+        return create_order_line(
+            order,
+            line_data,
+            manager,
+            discounts,
+            allocate_stock,
+        )
+
+
+def add_gift_cards_to_order(
+    checkout_info: "CheckoutInfo",
+    order: Order,
+    total_price_left: Money,
+    user: Optional[User],
+    app: Optional["App"],
+):
+    order_gift_cards = []
+    gift_cards_to_update = []
+    balance_data: List[Tuple[GiftCard, float]] = []
+    used_by_user = checkout_info.user
+    used_by_email = cast(str, checkout_info.get_customer_email())
+    for gift_card in checkout_info.checkout.gift_cards.select_for_update():
+        if total_price_left > zero_money(total_price_left.currency):
+            order_gift_cards.append(gift_card)
+
+            update_gift_card_balance(gift_card, total_price_left, balance_data)
+
+            set_gift_card_user(gift_card, used_by_user, used_by_email)
+
+            gift_card.last_used_on = timezone.now()
+            gift_cards_to_update.append(gift_card)
+
+    order.gift_cards.add(*order_gift_cards)
+    update_fields = [
+        "current_balance_amount",
+        "last_used_on",
+        "used_by",
+        "used_by_email",
+    ]
+    GiftCard.objects.bulk_update(gift_cards_to_update, update_fields)
+    gift_card_events.gift_cards_used_in_order_event(balance_data, order, user, app)
+
+
+def update_gift_card_balance(
+    gift_card: GiftCard,
+    total_price_left: Money,
+    balance_data: List[Tuple[GiftCard, float]],
+):
+    previous_balance = gift_card.current_balance
+    if total_price_left < gift_card.current_balance:
+        gift_card.current_balance = gift_card.current_balance - total_price_left
+        total_price_left = zero_money(total_price_left.currency)
+    else:
+        total_price_left = total_price_left - gift_card.current_balance
+        gift_card.current_balance_amount = 0
+    balance_data.append((gift_card, previous_balance.amount))
+
+
+def set_gift_card_user(
+    gift_card: GiftCard,
+    used_by_user: Optional[User],
+    used_by_email: str,
+):
+    """Set user when the gift card is used for the first time."""
+    if gift_card.used_by_email is None:
+        gift_card.used_by = (
+            used_by_user
+            if used_by_user
+            else User.objects.filter(email=used_by_email).first()
+        )
+        gift_card.used_by_email = used_by_email
+
+
+def _update_allocations_for_line(
+    line_info: OrderLineInfo,
+    old_quantity: int,
+    new_quantity: int,
+    channel: "Channel",
+    manager: "PluginsManager",
+):
+    if old_quantity == new_quantity:
+        return
+
+    if not get_order_lines_with_track_inventory([line_info]):
+        return
+
+    if old_quantity < new_quantity:
+        line_info.quantity = new_quantity - old_quantity
+        increase_allocations([line_info], channel, manager)
+    else:
+        line_info.quantity = old_quantity - new_quantity
+        decrease_allocations([line_info], manager)
+
+
+def change_order_line_quantity(
+    user,
+    app,
+    line_info,
+    old_quantity: int,
+    new_quantity: int,
+    channel: "Channel",
+    manager: "PluginsManager",
+    send_event=True,
+):
+    """Change the quantity of ordered items in a order line."""
+    line = line_info.line
+    if new_quantity:
+        if line.order.is_unconfirmed():
+            _update_allocations_for_line(
+                line_info, old_quantity, new_quantity, channel, manager
+            )
+        line.quantity = new_quantity
+        total_price_net_amount = line.quantity * line.unit_price_net_amount
+        total_price_gross_amount = line.quantity * line.unit_price_gross_amount
+        line.total_price_net_amount = total_price_net_amount.quantize(Decimal("0.001"))
+        line.total_price_gross_amount = total_price_gross_amount.quantize(
+            Decimal("0.001")
+        )
+        undiscounted_total_price_gross_amount = (
+            line.quantity * line.undiscounted_unit_price_gross_amount
+        )
+        undiscounted_total_price_net_amount = (
+            line.quantity * line.undiscounted_unit_price_net_amount
+        )
+        line.undiscounted_total_price_gross_amount = (
+            undiscounted_total_price_gross_amount.quantize(Decimal("0.001"))
+        )
+        line.undiscounted_total_price_net_amount = (
+            undiscounted_total_price_net_amount.quantize(Decimal("0.001"))
+        )
+        line.save(
+            update_fields=[
+                "quantity",
+                "total_price_net_amount",
+                "total_price_gross_amount",
+                "undiscounted_total_price_gross_amount",
+                "undiscounted_total_price_net_amount",
+            ]
+        )
+    else:
+        delete_order_line(line_info, manager)
+
+    quantity_diff = old_quantity - new_quantity
+
+    if send_event:
+        create_order_event(line, user, app, quantity_diff)
+
+
+def create_order_event(line, user, app, quantity_diff):
+    if quantity_diff > 0:
+        events.order_removed_products_event(
+            order=line.order,
+            user=user,
+            app=app,
+            order_lines=[line],
+            quantity_diff=quantity_diff,
+        )
+    elif quantity_diff < 0:
+        events.order_added_products_event(
+            order=line.order,
+            user=user,
+            app=app,
+            order_lines=[line],
+            quantity_diff=quantity_diff * -1,
+        )
+
+
+def delete_order_line(line_info, manager):
+    """Delete an order line from an order."""
+    if line_info.line.order.is_unconfirmed():
+        decrease_allocations([line_info], manager)
+    line_info.line.delete()
+
+
+def restock_fulfillment_lines(fulfillment, warehouse):
+    """Return fulfilled products to corresponding stocks.
+
+    Return products to stocks and update order lines quantity fulfilled values.
+    """
+    order_lines = []
+    for line in fulfillment:
+        if line.order_line.variant and line.order_line.variant.track_inventory:
+            increase_stock(line.order_line, warehouse, line.quantity, allocate=True)
+        order_line = line.order_line
+        order_line.quantity_fulfilled -= line.quantity
+        order_lines.append(order_line)
+    OrderLine.objects.bulk_update(order_lines, ["quantity_fulfilled"])
+
+
+def sum_order_totals(qs, currency_code):
+    zero = Money(0, currency=currency_code)
+    taxed_zero = TaxedMoney(zero, zero)
+    return sum([order.total for order in qs], taxed_zero)
+
+
+def get_all_shipping_methods_for_order(
+    order: Order,
+    shipping_channel_listings: Iterable["ShippingMethodChannelListing"],
+) -> List[ShippingMethodData]:
+    if not order.is_shipping_required():
+        return []
+
+    if not order.shipping_address:
+        return []
+
+    all_methods = []
+
+    shipping_methods = ShippingMethod.objects.applicable_shipping_methods_for_instance(
+        order,
+        channel_id=order.channel_id,
+        price=order.get_subtotal().gross,
+        country_code=order.shipping_address.country.code,
+    ).prefetch_related("channel_listings")
+
+    listing_map = {
+        listing.shipping_method_id: listing for listing in shipping_channel_listings
+    }
+
+    for method in shipping_methods:
+        listing = listing_map.get(method.id)
+        if listing:
+            shipping_method_data = convert_to_shipping_method_data(method, listing)
+            all_methods.append(shipping_method_data)
+    return all_methods
+
+
+def get_valid_shipping_methods_for_order(
+    order:
