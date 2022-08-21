2d122eb9552d2045d9dd4931baa321cbc3c75229
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-08-21 09:49:33 +0000

    Update db.js

--- saleor/graphql/payment/tests/mutations/test_transaction_request_action.py
+++ saleor/graphql/payment/tests/mutations/test_transaction_request_action.py
@@ -441,4 +441,134 @@ def test_transaction_request_uses_handle_payment_permission(
     get_graphql_content(response)
 
     assert mocked_is_active.called
-    mo
+    mocked_payment_action_request.assert_called_once_with(
+        TransactionActionData(
+            transaction=transaction,
+            action_type=TransactionAction.REFUND,
+            action_value=refund_amount,
+        ),
+        channel_slug=checkout.channel.slug,
+    )
+
+
+@patch("saleor.plugins.manager.PluginsManager.is_event_active_for_any_plugin")
+@patch("saleor.plugins.manager.PluginsManager.transaction_action_request")
+def test_transaction_request_uses_manage_orders_permission(
+    mocked_payment_action_request,
+    mocked_is_active,
+    order,
+    app_api_client,
+    permission_manage_orders,
+):
+    # given
+    mocked_is_active.return_value = True
+
+    transaction = TransactionItem.objects.create(
+        status="Captured",
+        type="Credit card",
+        reference="PSP ref",
+        available_actions=["refund"],
+        currency="USD",
+        order_id=order.pk,
+        charged_value=Decimal("10"),
+    )
+    refund_amount = Decimal("1")
+    variables = {
+        "id": graphene.Node.to_global_id("TransactionItem", transaction.pk),
+        "action_type": TransactionActionEnum.REFUND.name,
+        "amount": refund_amount,
+    }
+
+    # when
+    response = app_api_client.post_graphql(
+        MUTATION_TRANSACTION_REQUEST_ACTION,
+        variables,
+        permissions=[permission_manage_orders],
+    )
+
+    # then
+    get_graphql_content(response)
+
+    assert mocked_is_active.called
+    mocked_payment_action_request.assert_called_once_with(
+        TransactionActionData(
+            transaction=transaction,
+            action_type=TransactionAction.REFUND,
+            action_value=refund_amount,
+        ),
+        channel_slug=order.channel.slug,
+    )
+
+
+def test_transaction_request_action_missing_permission(
+    app_api_client, order_with_lines
+):
+    # given
+
+    transaction = TransactionItem.objects.create(
+        status="Authorized",
+        type="Credit card",
+        reference="PSP ref",
+        available_actions=["charge", "void"],
+        currency="USD",
+        order_id=order_with_lines.pk,
+        authorized_value=Decimal("10"),
+    )
+    variables = {
+        "id": graphene.Node.to_global_id("TransactionItem", transaction.pk),
+        "action_type": TransactionActionEnum.VOID.name,
+    }
+
+    # when
+    response = app_api_client.post_graphql(
+        MUTATION_TRANSACTION_REQUEST_ACTION, variables, permissions=[]
+    )
+
+    # then
+    assert_no_permission(response)
+
+
+@patch("saleor.plugins.manager.PluginsManager.is_event_active_for_any_plugin")
+def test_transaction_request_action_missing_event(
+    mocked_is_active, staff_api_client, permission_manage_payments, order
+):
+    # given
+    authorization_value = Decimal("10")
+    transaction = TransactionItem.objects.create(
+        status="Authorized",
+        type="Credit card",
+        reference="PSP ref",
+        available_actions=["charge", "void"],
+        currency="USD",
+        order_id=order.pk,
+        authorized_value=authorization_value,
+    )
+    mocked_is_active.return_value = False
+
+    variables = {
+        "id": graphene.Node.to_global_id("TransactionItem", transaction.pk),
+        "action_type": TransactionActionEnum.VOID.name,
+    }
+
+    # when
+    response = staff_api_client.post_graphql(
+        MUTATION_TRANSACTION_REQUEST_ACTION,
+        variables,
+        permissions=[
+            permission_manage_payments,
+        ],
+    )
+
+    # then
+    content = get_graphql_content(response)
+    data = content["data"]["transactionRequestAction"]
+    assert len(data["errors"]) == 1
+    assert data["errors"][0]["message"] == (
+        "No app or plugin is configured to handle payment action requests."
+    )
+    code_enum = TransactionRequestActionErrorCode
+    assert data["errors"][0]["code"] == (
+        code_enum.MISSING_TRANSACTION_ACTION_REQUEST_WEBHOOK.name
+    )
+
+    assert mocked_is_active.called
