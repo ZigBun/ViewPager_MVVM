c5f4ee7ceffcd5ec7028e9d15b32a40853448753
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-10-25 09:19:22 +0000

    Google

--- saleor/account/migrations/0075_add_address_metadata.py
+++ saleor/account/migrations/0075_add_address_metadata.py
@@ -8,4 +8,39 @@ import saleor.core.utils.json_serializer
 class Migration(migrations.Migration):
     dependencies = [
         ("account", "0074_merge_20230102_0914"),
-   
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="address",
+            name="metadata",
+            field=models.JSONField(
+                blank=True,
+                default=dict,
+                encoder=saleor.core.utils.json_serializer.CustomJsonEncoder,
+                null=True,
+            ),
+        ),
+        migrations.AddField(
+            model_name="address",
+            name="private_metadata",
+            field=models.JSONField(
+                blank=True,
+                default=dict,
+                encoder=saleor.core.utils.json_serializer.CustomJsonEncoder,
+                null=True,
+            ),
+        ),
+        migrations.AddIndex(
+            model_name="address",
+            index=django.contrib.postgres.indexes.GinIndex(
+                fields=["private_metadata"], name="address_p_meta_idx"
+            ),
+        ),
+        migrations.AddIndex(
+            model_name="address",
+            index=django.contrib.postgres.indexes.GinIndex(
+                fields=["metadata"], name="address_meta_idx"
+            ),
+        ),
+    ]
