ca3b642b10a9b26259b96383a871b47e7ba987d2
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-11-06 14:25:31 +0000

    adjust padding

--- saleor/graphql/account/tests/test_account.py
+++ saleor/graphql/account/tests/test_account.py
@@ -180,4 +180,239 @@ FULL_USER_QUERY = """
                 isDefaultBillingAddress
             }
             avatar {
-   
+                url
+            }
+            userPermissions {
+                code
+                sourcePermissionGroups(userId: $id) {
+                    name
+                }
+            }
+            permissionGroups {
+                name
+                permissions {
+                    code
+                }
+            }
+            editableGroups {
+                name
+            }
+            giftCards(first: 10) {
+                edges {
+                    node {
+                        id
+                    }
+                }
+            }
+            checkouts(first: 10) {
+                edges {
+                    node {
+                        id
+                    }
+                }
+            }
+        }
+    }
+"""
+
+
+def test_query_customer_user(
+    staff_api_client,
+    customer_user,
+    gift_card_used,
+    gift_card_expiry_date,
+    address,
+    permission_manage_users,
+    permission_manage_orders,
+    media_root,
+    settings,
+    checkout,
+):
+    user = customer_user
+    user.default_shipping_address.country = "US"
+    user.default_shipping_address.save()
+    user.addresses.add(address.get_copy())
+
+    avatar_mock = MagicMock(spec=File)
+    avatar_mock.name = "image.jpg"
+    user.avatar = avatar_mock
+    user.save()
+
+    checkout.user = user
+    checkout.save()
+
+    Group.objects.create(name="empty group")
+
+    query = FULL_USER_QUERY
+    ID = graphene.Node.to_global_id("User", customer_user.id)
+    variables = {"id": ID}
+    staff_api_client.user.user_permissions.add(
+        permission_manage_users, permission_manage_orders
+    )
+    response = staff_api_client.post_graphql(query, variables)
+    content = get_graphql_content(response)
+    data = content["data"]["user"]
+    assert data["email"] == user.email
+    assert data["firstName"] == user.first_name
+    assert data["lastName"] == user.last_name
+    assert data["isStaff"] == user.is_staff
+    assert data["isActive"] == user.is_active
+    assert data["orders"]["totalCount"] == user.orders.count()
+    assert data["avatar"]["url"]
+    assert data["languageCode"] == settings.LANGUAGE_CODE.upper()
+    assert len(data["editableGroups"]) == 0
+
+    assert len(data["addresses"]) == user.addresses.count()
+    for address in data["addresses"]:
+        if address["isDefaultShippingAddress"]:
+            address_id = graphene.Node.to_global_id(
+                "Address", user.default_shipping_address.id
+            )
+            assert address["id"] == address_id
+        if address["isDefaultBillingAddress"]:
+            address_id = graphene.Node.to_global_id(
+                "Address", user.default_billing_address.id
+            )
+            assert address["id"] == address_id
+
+    address = data["defaultShippingAddress"]
+    user_address = user.default_shipping_address
+    assert address["firstName"] == user_address.first_name
+    assert address["lastName"] == user_address.last_name
+    assert address["companyName"] == user_address.company_name
+    assert address["streetAddress1"] == user_address.street_address_1
+    assert address["streetAddress2"] == user_address.street_address_2
+    assert address["city"] == user_address.city
+    assert address["cityArea"] == user_address.city_area
+    assert address["postalCode"] == user_address.postal_code
+    assert address["country"]["code"] == user_address.country.code
+    assert address["countryArea"] == user_address.country_area
+    assert address["phone"] == user_address.phone.as_e164
+    assert address["isDefaultShippingAddress"] is None
+    assert address["isDefaultBillingAddress"] is None
+
+    address = data["defaultBillingAddress"]
+    user_address = user.default_billing_address
+    assert address["firstName"] == user_address.first_name
+    assert address["lastName"] == user_address.last_name
+    assert address["companyName"] == user_address.company_name
+    assert address["streetAddress1"] == user_address.street_address_1
+    assert address["streetAddress2"] == user_address.street_address_2
+    assert address["city"] == user_address.city
+    assert address["cityArea"] == user_address.city_area
+    assert address["postalCode"] == user_address.postal_code
+    assert address["country"]["code"] == user_address.country.code
+    assert address["countryArea"] == user_address.country_area
+    assert address["phone"] == user_address.phone.as_e164
+    assert address["isDefaultShippingAddress"] is None
+    assert address["isDefaultBillingAddress"] is None
+    assert len(data["giftCards"]) == 1
+    assert data["giftCards"]["edges"][0]["node"]["id"] == graphene.Node.to_global_id(
+        "GiftCard", gift_card_used.pk
+    )
+    assert data["checkoutIds"] == [to_global_id_or_none(checkout)]
+    assert data["checkouts"]["edges"][0]["node"]["id"] == graphene.Node.to_global_id(
+        "Checkout", checkout.pk
+    )
+
+
+def test_query_customer_user_with_orders(
+    staff_api_client,
+    customer_user,
+    order_list,
+    permission_manage_users,
+    permission_manage_orders,
+):
+    # given
+    query = FULL_USER_QUERY
+    order_unfulfilled = order_list[0]
+    order_unfulfilled.user = customer_user
+
+    order_unconfirmed = order_list[1]
+    order_unconfirmed.status = OrderStatus.UNCONFIRMED
+    order_unconfirmed.user = customer_user
+
+    order_draft = order_list[2]
+    order_draft.status = OrderStatus.DRAFT
+    order_draft.user = customer_user
+
+    Order.objects.bulk_update(
+        [order_unconfirmed, order_draft, order_unfulfilled], ["user", "status"]
+    )
+
+    id = graphene.Node.to_global_id("User", customer_user.id)
+    variables = {"id": id}
+
+    # when
+    response = staff_api_client.post_graphql(
+        query,
+        variables,
+        permissions=[permission_manage_users, permission_manage_orders],
+    )
+
+    # then
+    content = get_graphql_content(response)
+    user = content["data"]["user"]
+    assert {order["node"]["id"] for order in user["orders"]["edges"]} == {
+        graphene.Node.to_global_id("Order", order.pk) for order in order_list
+    }
+
+
+def test_query_customer_user_with_orders_no_manage_orders_perm(
+    staff_api_client,
+    customer_user,
+    order_list,
+    permission_manage_users,
+):
+    # given
+    query = FULL_USER_QUERY
+    order_unfulfilled = order_list[0]
+    order_unfulfilled.user = customer_user
+
+    order_unconfirmed = order_list[1]
+    order_unconfirmed.status = OrderStatus.UNCONFIRMED
+    order_unconfirmed.user = customer_user
+
+    order_draft = order_list[2]
+    order_draft.status = OrderStatus.DRAFT
+    order_draft.user = customer_user
+
+    Order.objects.bulk_update(
+        [order_unconfirmed, order_draft, order_unfulfilled], ["user", "status"]
+    )
+
+    id = graphene.Node.to_global_id("User", customer_user.id)
+    variables = {"id": id}
+
+    # when
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+
+    # then
+    assert_no_permission(response)
+
+
+def test_query_customer_user_app(
+    app_api_client,
+    customer_user,
+    address,
+    permission_manage_users,
+    permission_manage_staff,
+    permission_manage_orders,
+    media_root,
+    app,
+):
+    user = customer_user
+    user.default_shipping_address.country = "US"
+    user.default_shipping_address.save()
+    user.addresses.add(address.get_copy())
+
+    avatar_mock = MagicMock(spec=File)
+    avatar_mock.name = "image.jpg"
+    user.avatar = avatar_mock
+    user.save()
+
+    Group.objects.create(name="empty group")
+
+    q
