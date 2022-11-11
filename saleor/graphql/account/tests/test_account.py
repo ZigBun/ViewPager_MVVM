382d09d7dc86c76b7b67958ad1fd930df085bff8
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-11-11 16:21:04 +0000

    Updated the ruamel.yaml feedstock.

--- saleor/graphql/account/tests/test_account.py
+++ saleor/graphql/account/tests/test_account.py
@@ -1651,4 +1651,234 @@ def test_customer_create(
         "channel_slug": channel_PLN.slug,
         **get_site_context_payload(site_settings.site),
     }
-    mock
+    mocked_notify.assert_called_once_with(
+        NotifyEventType.ACCOUNT_SET_CUSTOMER_PASSWORD,
+        payload=expected_payload,
+        channel_slug=channel_PLN.slug,
+    )
+
+    assert set([shipping_address, billing_address]) == set(new_user.addresses.all())
+    customer_creation_event = account_events.CustomerEvent.objects.get()
+    assert customer_creation_event.type == account_events.CustomerEvents.ACCOUNT_CREATED
+    assert customer_creation_event.user == new_customer
+
+
+@patch("saleor.account.notifications.default_token_generator.make_token")
+@patch("saleor.plugins.manager.PluginsManager.notify")
+def test_customer_create_send_password_with_url(
+    mocked_notify,
+    mocked_generator,
+    staff_api_client,
+    permission_manage_users,
+    channel_PLN,
+    site_settings,
+):
+    mocked_generator.return_value = "token"
+    email = "api_user@example.com"
+    variables = {
+        "email": email,
+        "redirect_url": "https://www.example.com",
+        "channel": channel_PLN.slug,
+    }
+
+    response = staff_api_client.post_graphql(
+        CUSTOMER_CREATE_MUTATION, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["customerCreate"]
+    assert not data["errors"]
+
+    new_customer = User.objects.get(email=email)
+    assert new_customer
+    redirect_url = "https://www.example.com"
+    params = urlencode({"email": email, "token": "token"})
+    password_set_url = prepare_url(params, redirect_url)
+    expected_payload = {
+        "user": get_default_user_payload(new_customer),
+        "password_set_url": password_set_url,
+        "token": "token",
+        "recipient_email": new_customer.email,
+        "channel_slug": channel_PLN.slug,
+        **get_site_context_payload(site_settings.site),
+    }
+    mocked_notify.assert_called_once_with(
+        NotifyEventType.ACCOUNT_SET_CUSTOMER_PASSWORD,
+        payload=expected_payload,
+        channel_slug=channel_PLN.slug,
+    )
+
+
+def test_customer_create_without_send_password(
+    staff_api_client, permission_manage_users
+):
+    email = "api_user@example.com"
+    variables = {"email": email}
+    response = staff_api_client.post_graphql(
+        CUSTOMER_CREATE_MUTATION, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["customerCreate"]
+    assert not data["errors"]
+    User.objects.get(email=email)
+
+
+def test_customer_create_with_invalid_url(staff_api_client, permission_manage_users):
+    email = "api_user@example.com"
+    variables = {"email": email, "redirect_url": "invalid"}
+    response = staff_api_client.post_graphql(
+        CUSTOMER_CREATE_MUTATION, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["customerCreate"]
+    assert data["errors"][0] == {
+        "field": "redirectUrl",
+        "code": AccountErrorCode.INVALID.name,
+        "message": ANY,
+    }
+    staff_user = User.objects.filter(email=email)
+    assert not staff_user
+
+
+def test_customer_create_with_not_allowed_url(
+    staff_api_client, permission_manage_users
+):
+    email = "api_user@example.com"
+    variables = {"email": email, "redirect_url": "https://www.fake.com"}
+    response = staff_api_client.post_graphql(
+        CUSTOMER_CREATE_MUTATION, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["customerCreate"]
+    assert data["errors"][0] == {
+        "field": "redirectUrl",
+        "code": AccountErrorCode.INVALID.name,
+        "message": ANY,
+    }
+    staff_user = User.objects.filter(email=email)
+    assert not staff_user
+
+
+def test_customer_create_with_upper_case_email(
+    staff_api_client, permission_manage_users
+):
+    # given
+    email = "UPPERCASE@example.com"
+    variables = {"email": email}
+
+    # when
+    response = staff_api_client.post_graphql(
+        CUSTOMER_CREATE_MUTATION, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+
+    # then
+    data = content["data"]["customerCreate"]
+    assert not data["errors"]
+    assert data["user"]["email"] == email.lower()
+
+
+def test_customer_create_with_non_unique_external_reference(
+    staff_api_client, permission_manage_users, customer_user
+):
+    # given
+    ext_ref = "test-ext-ref"
+    customer_user.external_reference = ext_ref
+    customer_user.save(update_fields=["external_reference"])
+
+    variables = {"email": "mail.test@exampale.com", "externalReference": ext_ref}
+
+    # when
+    response = staff_api_client.post_graphql(
+        CUSTOMER_CREATE_MUTATION, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+
+    # then
+    error = content["data"]["customerCreate"]["errors"][0]
+    assert error["field"] == "externalReference"
+    assert error["code"] == AccountErrorCode.UNIQUE.name
+    assert error["message"] == "User with this External reference already exists."
+
+
+def test_customer_update(
+    staff_api_client, staff_user, customer_user, address, permission_manage_users
+):
+    query = """
+    mutation UpdateCustomer(
+            $id: ID!, $firstName: String, $lastName: String,
+            $isActive: Boolean, $note: String, $billing: AddressInput,
+            $shipping: AddressInput, $languageCode: LanguageCodeEnum,
+            $externalReference: String
+        ) {
+        customerUpdate(
+            id: $id,
+            input: {
+                isActive: $isActive,
+                firstName: $firstName,
+                lastName: $lastName,
+                note: $note,
+                defaultBillingAddress: $billing
+                defaultShippingAddress: $shipping,
+                languageCode: $languageCode,
+                externalReference: $externalReference
+                }
+            ) {
+            errors {
+                field
+                message
+            }
+            user {
+                id
+                firstName
+                lastName
+                defaultBillingAddress {
+                    id
+                }
+                defaultShippingAddress {
+                    id
+                }
+                languageCode
+                isActive
+                note
+                externalReference
+            }
+        }
+    }
+    """
+
+    # this test requires addresses to be set and checks whether new address
+    # instances weren't created, but the existing ones got updated
+    assert customer_user.default_billing_address
+    assert customer_user.default_shipping_address
+    billing_address_pk = customer_user.default_billing_address.pk
+    shipping_address_pk = customer_user.default_shipping_address.pk
+
+    user_id = graphene.Node.to_global_id("User", customer_user.id)
+    first_name = "new_first_name"
+    last_name = "new_last_name"
+    note = "Test update note"
+    external_reference = "test-ext-ref"
+    address_data = convert_dict_keys_to_camel_case(address.as_data())
+    address_data.pop("metadata")
+    address_data.pop("privateMetadata")
+
+    new_street_address = "Updated street address"
+    address_data["streetAddress1"] = new_street_address
+
+    variables = {
+        "id": user_id,
+        "firstName": first_name,
+        "lastName": last_name,
+        "isActive": False,
+        "note": note,
+        "billing": address_data,
+        "shipping": address_data,
+        "languageCode": "PL",
+        "externalReference": external_reference,
+    }
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+
+    customer = User.objects.get(email=customer_user.email)
