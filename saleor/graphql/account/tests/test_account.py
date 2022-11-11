06bfc51276f52db0686d7536644b63c1420b3ee1
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-11-11 18:08:37 +0000

    ignore _site

--- saleor/graphql/account/tests/test_account.py
+++ saleor/graphql/account/tests/test_account.py
@@ -1882,3 +1882,245 @@ def test_customer_update(
     content = get_graphql_content(response)
 
     customer = User.objects.get(email=customer_user.email)
+
+    # check that existing instances are updated
+    shipping_address, billing_address = (
+        customer.default_shipping_address,
+        customer.default_billing_address,
+    )
+    assert billing_address.pk == billing_address_pk
+    assert shipping_address.pk == shipping_address_pk
+
+    assert billing_address.street_address_1 == new_street_address
+    assert shipping_address.street_address_1 == new_street_address
+
+    data = content["data"]["customerUpdate"]
+    assert data["errors"] == []
+    assert data["user"]["firstName"] == first_name
+    assert data["user"]["lastName"] == last_name
+    assert data["user"]["note"] == note
+    assert data["user"]["languageCode"] == "PL"
+    assert data["user"]["externalReference"] == external_reference
+    assert not data["user"]["isActive"]
+
+    (
+        name_changed_event,
+        deactivated_event,
+    ) = account_events.CustomerEvent.objects.order_by("pk")
+
+    assert name_changed_event.type == account_events.CustomerEvents.NAME_ASSIGNED
+    assert name_changed_event.user.pk == staff_user.pk
+    assert name_changed_event.parameters == {"message": customer.get_full_name()}
+
+    assert deactivated_event.type == account_events.CustomerEvents.ACCOUNT_DEACTIVATED
+    assert deactivated_event.user.pk == staff_user.pk
+    assert deactivated_event.parameters == {"account_id": customer_user.id}
+
+    customer_user.refresh_from_db()
+    assert (
+        generate_address_search_document_value(billing_address)
+        in customer_user.search_document
+    )
+    assert (
+        generate_address_search_document_value(shipping_address)
+        in customer_user.search_document
+    )
+
+
+UPDATE_CUSTOMER_BY_EXTERNAL_REFERENCE = """
+    mutation UpdateCustomer(
+        $id: ID, $externalReference: String, $input: CustomerInput!
+    ) {
+        customerUpdate(id: $id, externalReference: $externalReference, input: $input) {
+            errors {
+                field
+                message
+                code
+            }
+            user {
+                id
+                externalReference
+                firstName
+            }
+        }
+    }
+    """
+
+
+def test_customer_update_by_external_reference(
+    staff_api_client, customer_user, permission_manage_users
+):
+    # given
+    query = UPDATE_CUSTOMER_BY_EXTERNAL_REFERENCE
+    user = customer_user
+    new_name = "updated name"
+    ext_ref = "test-ext-ref"
+    user.external_reference = ext_ref
+    user.save(update_fields=["external_reference"])
+
+    variables = {
+        "externalReference": ext_ref,
+        "input": {"firstName": new_name},
+    }
+
+    # when
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+
+    # then
+    user.refresh_from_db()
+    data = content["data"]["customerUpdate"]
+    assert not data["errors"]
+    assert data["user"]["firstName"] == new_name == user.first_name
+    assert data["user"]["id"] == graphene.Node.to_global_id("User", user.id)
+    assert data["user"]["externalReference"] == ext_ref
+
+
+def test_update_customer_by_both_id_and_external_reference(
+    staff_api_client, customer_user, permission_manage_users
+):
+    # given
+    query = UPDATE_CUSTOMER_BY_EXTERNAL_REFERENCE
+    variables = {"input": {}, "externalReference": "whatever", "id": "whatever"}
+
+    # when
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+
+    # then
+    data = content["data"]["customerUpdate"]
+    assert not data["user"]
+    assert (
+        data["errors"][0]["message"]
+        == "Argument 'id' cannot be combined with 'external_reference'"
+    )
+
+
+def test_update_customer_by_external_reference_not_existing(
+    staff_api_client, customer_user, permission_manage_users
+):
+    # given
+    query = UPDATE_CUSTOMER_BY_EXTERNAL_REFERENCE
+    ext_ref = "non-existing-ext-ref"
+    variables = {
+        "input": {},
+        "externalReference": ext_ref,
+    }
+
+    # when
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+
+    # then
+    data = content["data"]["customerUpdate"]
+    assert not data["user"]
+    assert data["errors"][0]["message"] == f"Couldn't resolve to a node: {ext_ref}"
+    assert data["errors"][0]["field"] == "externalReference"
+
+
+def test_update_customer_with_non_unique_external_reference(
+    staff_api_client, permission_manage_users, user_list
+):
+    # given
+    query = UPDATE_CUSTOMER_BY_EXTERNAL_REFERENCE
+
+    ext_ref = "test-ext-ref"
+    user_1 = user_list[0]
+    user_1.external_reference = ext_ref
+    user_1.save(update_fields=["external_reference"])
+    user_2_id = graphene.Node.to_global_id("User", user_list[1].id)
+
+    variables = {"input": {"externalReference": ext_ref}, "id": user_2_id}
+
+    # when
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+
+    # then
+    error = content["data"]["customerUpdate"]["errors"][0]
+    assert error["field"] == "externalReference"
+    assert error["code"] == AccountErrorCode.UNIQUE.name
+    assert error["message"] == "User with this External reference already exists."
+
+
+UPDATE_CUSTOMER_EMAIL_MUTATION = """
+    mutation UpdateCustomer(
+            $id: ID!, $firstName: String, $lastName: String, $email: String) {
+        customerUpdate(id: $id, input: {
+            firstName: $firstName,
+            lastName: $lastName,
+            email: $email
+        }) {
+            errors {
+                field
+                message
+            }
+        }
+    }
+"""
+
+
+def test_customer_update_generates_event_when_changing_email(
+    staff_api_client, staff_user, customer_user, address, permission_manage_users
+):
+    query = UPDATE_CUSTOMER_EMAIL_MUTATION
+
+    user_id = graphene.Node.to_global_id("User", customer_user.id)
+    address_data = convert_dict_keys_to_camel_case(address.as_data())
+
+    new_street_address = "Updated street address"
+    address_data["streetAddress1"] = new_street_address
+
+    variables = {
+        "id": user_id,
+        "firstName": customer_user.first_name,
+        "lastName": customer_user.last_name,
+        "email": "mirumee@example.com",
+    }
+    staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+
+    # The email was changed, an event should have been triggered
+    email_changed_event = account_events.CustomerEvent.objects.get()
+    assert email_changed_event.type == account_events.CustomerEvents.EMAIL_ASSIGNED
+    assert email_changed_event.user.pk == staff_user.pk
+    assert email_changed_event.parameters == {"message": "mirumee@example.com"}
+
+
+UPDATE_CUSTOMER_IS_ACTIVE_MUTATION = """
+    mutation UpdateCustomer(
+        $id: ID!, $isActive: Boolean) {
+            customerUpdate(id: $id, input: {
+            isActive: $isActive,
+        }) {
+            errors {
+                field
+                message
+            }
+        }
+    }
+"""
+
+
+def test_customer_update_generates_event_when_deactivating(
+    staff_api_client, staff_user, customer_user, address, permission_manage_users
+):
+    query = UPDATE_CUSTOMER_IS_ACTIVE_MUTATION
+
+    user_id = graphene.Node.to_global_id("User", customer_user.id)
+
+    variables = {"id": user_id, "isActive": False}
+    staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+
+    account_deactivated_event = account_events.CustomerEvent.
