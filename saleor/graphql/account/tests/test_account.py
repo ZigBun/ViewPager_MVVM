9900e7a1f113d5103342999db31f3370e8679468
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-11-20 12:15:58 +0000

    Github

--- saleor/graphql/account/tests/test_account.py
+++ saleor/graphql/account/tests/test_account.py
@@ -4421,3 +4421,234 @@ def test_password_change_incorrect_old_password(user_api_client):
     customer_user.refresh_from_db()
     assert customer_user.check_password("password")
     assert data["errors"]
+    assert data["errors"][0]["field"] == "oldPassword"
+
+
+def test_password_change_invalid_new_password(user_api_client, settings):
+    settings.AUTH_PASSWORD_VALIDATORS = [
+        {
+            "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+            "OPTIONS": {"min_length": 5},
+        },
+        {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+    ]
+
+    customer_user = user_api_client.user
+    variables = {"oldPassword": "password", "newPassword": "1234"}
+    response = user_api_client.post_graphql(CHANGE_PASSWORD_MUTATION, variables)
+    content = get_graphql_content(response)
+    errors = content["data"]["passwordChange"]["errors"]
+    customer_user.refresh_from_db()
+    assert customer_user.check_password("password")
+    assert len(errors) == 2
+    assert errors[1]["field"] == "newPassword"
+    assert (
+        errors[0]["message"]
+        == "This password is too short. It must contain at least 5 characters."
+    )
+    assert errors[1]["field"] == "newPassword"
+    assert errors[1]["message"] == "This password is entirely numeric."
+
+
+def test_password_change_user_unusable_password_fails_if_old_password_is_set(
+    user_api_client,
+):
+    customer_user = user_api_client.user
+    customer_user.set_unusable_password()
+    customer_user.save()
+
+    new_password = "spanish-inquisition"
+
+    variables = {"oldPassword": "password", "newPassword": new_password}
+    response = user_api_client.post_graphql(CHANGE_PASSWORD_MUTATION, variables)
+    content = get_graphql_content(response)
+    data = content["data"]["passwordChange"]
+    assert data["errors"]
+    assert data["errors"][0]["field"] == "oldPassword"
+
+    customer_user.refresh_from_db()
+    assert not customer_user.has_usable_password()
+
+
+def test_password_change_user_unusable_password_if_old_password_is_omitted(
+    user_api_client,
+):
+    customer_user = user_api_client.user
+    customer_user.set_unusable_password()
+    customer_user.save()
+
+    new_password = "spanish-inquisition"
+
+    variables = {"oldPassword": None, "newPassword": new_password}
+    response = user_api_client.post_graphql(CHANGE_PASSWORD_MUTATION, variables)
+    content = get_graphql_content(response)
+    data = content["data"]["passwordChange"]
+    assert not data["errors"]
+    assert data["user"]["email"] == customer_user.email
+
+    customer_user.refresh_from_db()
+    assert customer_user.check_password(new_password)
+
+    password_change_event = account_events.CustomerEvent.objects.get()
+    assert password_change_event.type == account_events.CustomerEvents.PASSWORD_CHANGED
+    assert password_change_event.user == customer_user
+
+
+def test_password_change_user_usable_password_fails_if_old_password_is_omitted(
+    user_api_client,
+):
+    customer_user = user_api_client.user
+
+    new_password = "spanish-inquisition"
+
+    variables = {"newPassword": new_password}
+    response = user_api_client.post_graphql(CHANGE_PASSWORD_MUTATION, variables)
+    content = get_graphql_content(response)
+    data = content["data"]["passwordChange"]
+    assert data["errors"]
+    assert data["errors"][0]["field"] == "oldPassword"
+
+    customer_user.refresh_from_db()
+    assert customer_user.has_usable_password()
+    assert not customer_user.check_password(new_password)
+
+
+ADDRESS_CREATE_MUTATION = """
+    mutation CreateUserAddress($user: ID!, $address: AddressInput!) {
+        addressCreate(userId: $user, input: $address) {
+            errors {
+                field
+                message
+            }
+            address {
+                id
+                city
+                country {
+                    code
+                }
+            }
+            user {
+                id
+            }
+        }
+    }
+"""
+
+
+def test_create_address_mutation(
+    staff_api_client, customer_user, permission_manage_users, graphql_address_data
+):
+    # given
+    query = ADDRESS_CREATE_MUTATION
+    graphql_address_data["city"] = "Dummy"
+    user_id = graphene.Node.to_global_id("User", customer_user.id)
+    variables = {"user": user_id, "address": graphql_address_data}
+    # when
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+    # then
+    assert content["data"]["addressCreate"]["errors"] == []
+    data = content["data"]["addressCreate"]
+    assert data["address"]["city"] == "DUMMY"
+    assert data["address"]["country"]["code"] == "PL"
+    address_obj = Address.objects.get(city="DUMMY")
+    assert address_obj.user_addresses.first() == customer_user
+    assert data["user"]["id"] == user_id
+
+    customer_user.refresh_from_db()
+    for field in ["city", "country"]:
+        assert variables["address"][field].lower() in customer_user.search_document
+
+
+@freeze_time("2022-05-12 12:00:00")
+@patch("saleor.plugins.webhook.plugin.get_webhooks_for_event")
+@patch("saleor.plugins.webhook.plugin.trigger_webhooks_async")
+def test_create_address_mutation_trigger_webhook(
+    mocked_webhook_trigger,
+    mocked_get_webhooks_for_event,
+    any_webhook,
+    staff_api_client,
+    customer_user,
+    permission_manage_users,
+    settings,
+    graphql_address_data,
+):
+    # given
+    mocked_get_webhooks_for_event.return_value = [any_webhook]
+    settings.PLUGINS = ["saleor.plugins.webhook.plugin.WebhookPlugin"]
+
+    user_id = graphene.Node.to_global_id("User", customer_user.id)
+    variables = {"user": user_id, "address": graphql_address_data}
+
+    # when
+    response = staff_api_client.post_graphql(
+        ADDRESS_CREATE_MUTATION, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+    address = Address.objects.last()
+
+    # then
+    assert not content["data"]["addressCreate"]["errors"]
+    assert content["data"]["addressCreate"]
+
+    mocked_webhook_trigger.assert_called_once_with(
+        *generate_address_webhook_call_args(
+            address,
+            WebhookEventAsyncType.ADDRESS_CREATED,
+            staff_api_client.user,
+            any_webhook,
+        )
+    )
+
+
+@override_settings(MAX_USER_ADDRESSES=2)
+def test_create_address_mutation_the_oldest_address_is_deleted(
+    staff_api_client,
+    customer_user,
+    address,
+    permission_manage_users,
+    graphql_address_data,
+):
+    # given
+    same_address = Address.objects.create(**address.as_data())
+    customer_user.addresses.set([address, same_address])
+
+    user_addresses_count = customer_user.addresses.count()
+    graphql_address_data["city"] = "Dummy"
+    query = ADDRESS_CREATE_MUTATION
+    user_id = graphene.Node.to_global_id("User", customer_user.id)
+    variables = {"user": user_id, "address": graphql_address_data}
+
+    # when
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+
+    # then
+    assert content["data"]["addressCreate"]["errors"] == []
+    data = content["data"]["addressCreate"]
+    assert data["address"]["city"] == "DUMMY"
+    assert data["address"]["country"]["code"] == "PL"
+    address_obj = Address.objects.get(city="DUMMY")
+    assert address_obj.user_addresses.first() == customer_user
+    assert data["user"]["id"] == user_id
+
+    customer_user.refresh_from_db()
+    assert customer_user.addresses.count() == user_addresses_count
+
+    with pytest.raises(address._meta.model.DoesNotExist):
+        address.refresh_from_db()
+
+
+def test_create_address_validation_fails(
+    staff_api_client,
+    customer_user,
+    graphql_address_data,
+    permission_manage_users,
+    address,
+):
+    # given
+    query
