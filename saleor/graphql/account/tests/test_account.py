4bc6c0f1c3bbc135de05e7452da9bcdf8ce49010
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-11-23 06:01:54 +0000

    f2fs: add an sysfs entry to control the directory level

--- saleor/graphql/account/tests/test_account.py
+++ saleor/graphql/account/tests/test_account.py
@@ -4892,4 +4892,246 @@ def test_customer_update_own_address_not_updated_when_validation_fails(
     user_api_client.post_graphql(query, variables)
     address_obj.refresh_from_db()
     assert address_obj.city != address_data["city"]
-    assert 
+    assert address_obj.postal_code != address_data["postalCode"]
+
+
+@pytest.mark.parametrize(
+    "query", [ADDRESS_UPDATE_MUTATION, ACCOUNT_ADDRESS_UPDATE_MUTATION]
+)
+def test_customer_update_address_for_other(
+    user_api_client, customer_user, address_other_country, graphql_address_data, query
+):
+    address_obj = address_other_country
+    assert customer_user not in address_obj.user_addresses.all()
+
+    address_data = graphql_address_data
+    variables = {
+        "addressId": graphene.Node.to_global_id("Address", address_obj.id),
+        "address": address_data,
+    }
+    response = user_api_client.post_graphql(query, variables)
+    assert_no_permission(response)
+
+
+ADDRESS_DELETE_MUTATION = """
+    mutation deleteUserAddress($id: ID!) {
+        addressDelete(id: $id) {
+            address {
+                city
+            }
+            user {
+                id
+            }
+        }
+    }
+"""
+
+
+def test_address_delete_mutation(
+    staff_api_client, customer_user, permission_manage_users
+):
+    query = ADDRESS_DELETE_MUTATION
+    address_obj = customer_user.addresses.first()
+    variables = {"id": graphene.Node.to_global_id("Address", address_obj.id)}
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["addressDelete"]
+    assert data["address"]["city"] == address_obj.city
+    assert data["user"]["id"] == graphene.Node.to_global_id("User", customer_user.pk)
+    with pytest.raises(address_obj._meta.model.DoesNotExist):
+        address_obj.refresh_from_db()
+
+    customer_user.refresh_from_db()
+    assert (
+        generate_address_search_document_value(address_obj)
+        not in customer_user.search_document
+    )
+
+
+@freeze_time("2022-05-12 12:00:00")
+@patch("saleor.plugins.webhook.plugin.get_webhooks_for_event")
+@patch("saleor.plugins.webhook.plugin.trigger_webhooks_async")
+def test_address_delete_mutation_trigger_webhook(
+    mocked_webhook_trigger,
+    mocked_get_webhooks_for_event,
+    any_webhook,
+    staff_api_client,
+    customer_user,
+    permission_manage_users,
+    settings,
+):
+    # given
+    mocked_get_webhooks_for_event.return_value = [any_webhook]
+    settings.PLUGINS = ["saleor.plugins.webhook.plugin.WebhookPlugin"]
+
+    address = customer_user.addresses.first()
+    variables = {"id": graphene.Node.to_global_id("Address", address.id)}
+
+    # when
+    response = staff_api_client.post_graphql(
+        ADDRESS_DELETE_MUTATION, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+
+    # then
+    assert content["data"]["addressDelete"]
+    mocked_webhook_trigger.assert_called_once_with(
+        *generate_address_webhook_call_args(
+            address,
+            WebhookEventAsyncType.ADDRESS_DELETED,
+            staff_api_client.user,
+            any_webhook,
+        )
+    )
+
+
+def test_address_delete_mutation_as_app(
+    app_api_client, customer_user, permission_manage_users
+):
+    query = ADDRESS_DELETE_MUTATION
+    address_obj = customer_user.addresses.first()
+    variables = {"id": graphene.Node.to_global_id("Address", address_obj.id)}
+    response = app_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["addressDelete"]
+    assert data["address"]["city"] == address_obj.city
+    assert data["user"]["id"] == graphene.Node.to_global_id("User", customer_user.pk)
+    with pytest.raises(address_obj._meta.model.DoesNotExist):
+        address_obj.refresh_from_db()
+
+
+ACCOUNT_ADDRESS_DELETE_MUTATION = """
+    mutation deleteUserAddress($id: ID!) {
+        accountAddressDelete(id: $id) {
+            address {
+                city
+            }
+            user {
+                id
+            }
+        }
+    }
+"""
+
+
+def test_customer_delete_own_address(user_api_client, customer_user):
+    query = ACCOUNT_ADDRESS_DELETE_MUTATION
+    address_obj = customer_user.addresses.first()
+    user = user_api_client.user
+    variables = {"id": graphene.Node.to_global_id("Address", address_obj.id)}
+    response = user_api_client.post_graphql(query, variables)
+    content = get_graphql_content(response)
+    data = content["data"]["accountAddressDelete"]
+    assert data["address"]["city"] == address_obj.city
+    with pytest.raises(address_obj._meta.model.DoesNotExist):
+        address_obj.refresh_from_db()
+    user.refresh_from_db()
+    assert (
+        generate_address_search_document_value(address_obj) not in user.search_document
+    )
+
+
+@freeze_time("2022-05-12 12:00:00")
+@patch("saleor.plugins.webhook.plugin.get_webhooks_for_event")
+@patch("saleor.plugins.webhook.plugin.trigger_webhooks_async")
+def test_customer_delete_address_trigger_webhook(
+    mocked_webhook_trigger,
+    mocked_get_webhooks_for_event,
+    any_webhook,
+    user_api_client,
+    customer_user,
+    settings,
+):
+    # given
+    mocked_get_webhooks_for_event.return_value = [any_webhook]
+    settings.PLUGINS = ["saleor.plugins.webhook.plugin.WebhookPlugin"]
+
+    address = customer_user.addresses.first()
+    variables = {"id": graphene.Node.to_global_id("Address", address.id)}
+
+    # when
+    response = user_api_client.post_graphql(ACCOUNT_ADDRESS_DELETE_MUTATION, variables)
+    content = get_graphql_content(response)
+
+    # then
+    assert content["data"]["accountAddressDelete"]
+    mocked_webhook_trigger.assert_called_with(
+        *generate_address_webhook_call_args(
+            address,
+            WebhookEventAsyncType.ADDRESS_DELETED,
+            user_api_client.user,
+            any_webhook,
+        )
+    )
+
+
+@pytest.mark.parametrize(
+    "query", [ADDRESS_DELETE_MUTATION, ACCOUNT_ADDRESS_DELETE_MUTATION]
+)
+def test_customer_delete_address_for_other(
+    user_api_client, customer_user, address_other_country, query
+):
+    address_obj = address_other_country
+    assert customer_user not in address_obj.user_addresses.all()
+    variables = {"id": graphene.Node.to_global_id("Address", address_obj.id)}
+    response = user_api_client.post_graphql(query, variables)
+    assert_no_permission(response)
+    address_obj.refresh_from_db()
+
+
+SET_DEFAULT_ADDRESS_MUTATION = """
+mutation($address_id: ID!, $user_id: ID!, $type: AddressTypeEnum!) {
+  addressSetDefault(addressId: $address_id, userId: $user_id, type: $type) {
+    errors {
+      field
+      message
+    }
+    user {
+      defaultBillingAddress {
+        id
+      }
+      defaultShippingAddress {
+        id
+      }
+    }
+  }
+}
+"""
+
+
+def test_set_default_address(
+    staff_api_client, address_other_country, customer_user, permission_manage_users
+):
+    customer_user.default_billing_address = None
+    customer_user.default_shipping_address = None
+    customer_user.save()
+
+    # try to set an address that doesn't belong to that user
+    address = address_other_country
+
+    variables = {
+        "address_id": graphene.Node.to_global_id("Address", address.id),
+        "user_id": graphene.Node.to_global_id("User", customer_user.id),
+        "type": AddressType.SHIPPING.upper(),
+    }
+
+    response = staff_api_client.post_graphql(
+        SET_DEFAULT_ADDRESS_MUTATION, variables, permissions=[permission_manage_users]
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["addressSetDefault"]
+    assert data["errors"][0]["field"] == "addressId"
+
+    # try to set a new billing address using one of user's addresses
+    address = customer_user.addresses.first()
+    address_id = graphene.Node.to_global_id("Address", address.id)
+
+    variables["address_id"] = address_id
+    response = staff_api_client.post_graphql(SET_DEFAULT_ADDRESS_MUTATION, variables)
+    content = get_graphql_content(response)
+    data = content["data"]["addressSetDefault"]
+    assert data["user"]["defaultShippingAddress"]["id"] == addre
