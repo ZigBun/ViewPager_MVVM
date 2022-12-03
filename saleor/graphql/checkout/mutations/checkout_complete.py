968dc8101bf11e06bfc44b9062c6076b26bc2bcc
ZigBun <109135201+ZigBun@users.noreply.github.com>
2022-12-03 15:26:34 +0000

    Release v3.0.0

--- saleor/graphql/checkout/mutations/checkout_complete.py
+++ saleor/graphql/checkout/mutations/checkout_complete.py
@@ -78,4 +78,206 @@ class CheckoutComplete(BaseMutation, I18nMixin):
                 f"{DEPRECATED_IN_3X_INPUT} Use checkoutPaymentCreate for this action."
             ),
         )
-        redirect_url = grap
+        redirect_url = graphene.String(
+            required=False,
+            description=(
+                "URL of a view where users should be redirected to "
+                "see the order details. URL in RFC 1808 format."
+            ),
+        )
+        payment_data = JSONString(
+            required=False,
+            description=(
+                "Client-side generated data required to finalize the payment."
+            ),
+        )
+        metadata = NonNullList(
+            MetadataInput,
+            description=(
+                "Fields required to update the checkout metadata." + ADDED_IN_38
+            ),
+            required=False,
+        )
+
+    class Meta:
+        description = (
+            "Completes the checkout. As a result a new order is created and "
+            "a payment charge is made. This action requires a successful "
+            "payment before it can be performed. "
+            "In case additional confirmation step as 3D secure is required "
+            "confirmationNeeded flag will be set to True and no order created "
+            "until payment is confirmed with second call of this mutation."
+        )
+        error_type_class = CheckoutError
+        error_type_field = "checkout_errors"
+
+    @classmethod
+    def validate_checkout_addresses(
+        cls,
+        checkout_info: CheckoutInfo,
+        lines: Iterable[CheckoutLineInfo],
+    ):
+        """Validate checkout addresses.
+
+        Mutations for updating addresses have option to turn off a validation. To keep
+        consistency, we need to validate it. This will confirm that we have a correct
+        address and we can finalize a checkout. In case when address fields
+        normalization was turned off, we apply it here.
+        Raises ValidationError when any address is not correct.
+        """
+        shipping_address = checkout_info.shipping_address
+        billing_address = checkout_info.billing_address
+
+        if is_shipping_required(lines):
+            clean_checkout_shipping(checkout_info, lines, CheckoutErrorCode)
+            if shipping_address:
+                shipping_address_data = shipping_address.as_data()
+                cls.validate_address(
+                    shipping_address_data,
+                    address_type=AddressType.SHIPPING,
+                    format_check=True,
+                    required_check=True,
+                    enable_normalization=True,
+                    instance=shipping_address,
+                )
+                if shipping_address_data != shipping_address.as_data():
+                    shipping_address.save()
+
+        if not billing_address:
+            raise ValidationError(
+                {
+                    "billing_address": ValidationError(
+                        "Billing address is not set",
+                        code=CheckoutErrorCode.BILLING_ADDRESS_NOT_SET.value,
+                    )
+                }
+            )
+        billing_address_data = billing_address.as_data()
+        cls.validate_address(
+            billing_address_data,
+            address_type=AddressType.BILLING,
+            format_check=True,
+            required_check=True,
+            enable_normalization=True,
+            instance=billing_address,
+        )
+        if billing_address_data != billing_address.as_data():
+            billing_address.save()
+
+    @classmethod
+    def perform_mutation(  # type: ignore[override]
+        cls,
+        _root,
+        info: ResolveInfo,
+        /,
+        *,
+        checkout_id=None,
+        id=None,
+        metadata=None,
+        payment_data=None,
+        redirect_url=None,
+        store_source,
+        token=None,
+    ):
+        # DEPRECATED
+        validate_one_of_args_is_in_mutation(
+            "checkout_id", checkout_id, "token", token, "id", id
+        )
+        tracking_code = analytics.get_client_id(info.context)
+
+        try:
+            checkout = get_checkout(
+                cls, info, checkout_id=checkout_id, token=token, id=id
+            )
+        except ValidationError as e:
+            # DEPRECATED
+            if id or checkout_id:
+                id = id or checkout_id
+                token = cls.get_global_id_or_error(
+                    id, only_type=Checkout, field="id" if id else "checkout_id"
+                )
+
+            order = order_models.Order.objects.get_by_checkout_token(token)
+            if order:
+                if not order.channel.is_active:
+                    raise ValidationError(
+                        {
+                            "channel": ValidationError(
+                                "Cannot complete checkout with inactive channel.",
+                                code=CheckoutErrorCode.CHANNEL_INACTIVE.value,
+                            )
+                        }
+                    )
+                # The order is already created. We return it as a success
+                # checkoutComplete response. Order is anonymized for not logged in
+                # user
+                return CheckoutComplete(
+                    order=order, confirmation_needed=False, confirmation_data={}
+                )
+            raise e
+        if metadata is not None:
+            cls.check_metadata_permissions(
+                info,
+                id or checkout_id or graphene.Node.to_global_id("Checkout", token),
+            )
+            cls.validate_metadata_keys(metadata)
+
+        validate_checkout_email(checkout)
+
+        manager = get_plugin_manager_promise(info.context).get()
+        lines, unavailable_variant_pks = fetch_checkout_lines(checkout)
+        if unavailable_variant_pks:
+            not_available_variants_ids = {
+                graphene.Node.to_global_id("ProductVariant", pk)
+                for pk in unavailable_variant_pks
+            }
+            raise ValidationError(
+                {
+                    "lines": ValidationError(
+                        "Some of the checkout lines variants are unavailable.",
+                        code=CheckoutErrorCode.UNAVAILABLE_VARIANT_IN_CHANNEL.value,
+                        params={"variants": not_available_variants_ids},
+                    )
+                }
+            )
+        if not lines:
+            raise ValidationError(
+                {
+                    "lines": ValidationError(
+                        "Cannot complete checkout without lines.",
+                        code=CheckoutErrorCode.NO_LINES.value,
+                    )
+                }
+            )
+        discounts = load_discounts(info.context)
+        checkout_info = fetch_checkout_info(checkout, lines, discounts, manager)
+
+        cls.validate_checkout_addresses(checkout_info, lines)
+
+        requestor = get_user_or_app_from_context(info.context)
+        if requestor and requestor.has_perm(AccountPermissions.IMPERSONATE_USER):
+            # Allow impersonating user and process a checkout by using user details
+            # assigned to checkout.
+            customer = checkout.user
+        else:
+            customer = info.context.user
+
+        site = get_site_promise(info.context).get()
+
+        order, action_required, action_data = complete_checkout(
+            manager=manager,
+            checkout_info=checkout_info,
+            lines=lines,
+            payment_data=payment_data or {},
+            store_source=store_source,
+            discounts=discounts,
+            user=customer,
+            app=get_app_promise(info.context).get(),
+            site_settings=site.settings,
+            tracking_code=tracking_code,
+            redirect_url=redirect_url,
+            metadata_list=metadata,
+        )
+
+        # If gateway returns information that additional steps are required we need
+    
