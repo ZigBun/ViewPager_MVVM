b701c526078c1c6c52240bb9b41571b9defa81dc
ZigBun <109135201+ZigBun@users.noreply.github.com>
2023-02-23 09:26:40 +0000

    Remove old stuff.

--- saleor/graphql/order/tests/mutations/test_order_bulk_cancel.py
+++ saleor/graphql/order/tests/mutations/test_order_bulk_cancel.py
@@ -90,4 +90,50 @@ def test_order_bulk_cancel_as_app(
     response = app_api_client.post_graphql(
         MUTATION_ORDER_BULK_CANCEL, variables, permissions=[permission_manage_orders]
     )
-    content = get_graphql_content(response
+    content = get_graphql_content(response)
+    data = content["data"]["orderBulkCancel"]
+    assert data["count"] == expected_count
+    assert not data["errors"]
+
+    calls = [
+        call(order=order, user=None, app=app_api_client.app, manager=ANY)
+        for order in orders
+    ]
+
+    mock_cancel_order.assert_has_calls(calls, any_order=True)
+    assert mock_cancel_order.call_count == expected_count
+
+
+@patch("saleor.graphql.order.bulk_mutations.orders.cancel_order")
+def test_order_bulk_cancel_without_sku(
+    mock_cancel_order,
+    staff_api_client,
+    order_list,
+    fulfilled_order_with_all_cancelled_fulfillments,
+    permission_manage_orders,
+    address,
+):
+    ProductVariant.objects.update(sku=None)
+    OrderLine.objects.update(product_sku=None)
+
+    orders = order_list
+    orders.append(fulfilled_order_with_all_cancelled_fulfillments)
+    expected_count = sum(order.can_cancel() for order in orders)
+    variables = {
+        "ids": [graphene.Node.to_global_id("Order", order.id) for order in orders],
+    }
+    response = staff_api_client.post_graphql(
+        MUTATION_ORDER_BULK_CANCEL, variables, permissions=[permission_manage_orders]
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["orderBulkCancel"]
+    assert data["count"] == expected_count
+    assert not data["errors"]
+
+    calls = [
+        call(order=order, user=staff_api_client.user, app=None, manager=ANY)
+        for order in orders
+    ]
+
+    mock_cancel_order.assert_has_calls(calls, any_order=True)
+    mock_cancel_order.call_count == expected_count
