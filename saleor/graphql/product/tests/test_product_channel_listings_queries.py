a297d39cd2fe29f8c691cf658bb4a5f83e275109
ZigBun <109135201+ZigBun@users.noreply.github.com>
2023-02-24 10:24:39 +0000

    key fix

--- saleor/graphql/product/tests/test_product_channel_listings_queries.py
+++ saleor/graphql/product/tests/test_product_channel_listings_queries.py
@@ -252,4 +252,202 @@ def test_product_pricing(
     net_DE,
     gross_DE,
     prices_entered_with_tax,
-    
+    permission_manage_products,
+):
+    # given
+    product = product_available_in_many_channels
+    _enable_flat_rates(channel_PLN, prices_entered_with_tax)
+    _configure_tax_rates(product)
+
+    # when
+    variables = {
+        "id": graphene.Node.to_global_id("Product", product.id),
+        "channel": channel_PLN.slug,
+    }
+    response = staff_api_client.post_graphql(
+        QUERY_PRODUCT_PRICING,
+        variables,
+        permissions=(permission_manage_products,),
+        check_no_permissions=False,
+    )
+    content = get_graphql_content(response)
+
+    # then
+    data = None
+    for channel_data in content["data"]["product"]["channelListings"]:
+        if channel_data["channel"]["slug"] == channel_PLN.slug:
+            data = channel_data
+
+    price_range_PL = data["pricingPL"]["priceRange"]
+    price_range_undiscounted_PL = data["pricingPL"]["priceRangeUndiscounted"]
+    assert price_range_PL["start"]["net"]["amount"] == net_PL
+    assert price_range_PL["start"]["gross"]["amount"] == gross_PL
+    assert price_range_PL["stop"]["net"]["amount"] == net_PL
+    assert price_range_PL["stop"]["gross"]["amount"] == gross_PL
+    assert price_range_undiscounted_PL["start"]["net"]["amount"] == net_PL
+    assert price_range_undiscounted_PL["start"]["gross"]["amount"] == gross_PL
+    assert price_range_undiscounted_PL["stop"]["net"]["amount"] == net_PL
+    assert price_range_undiscounted_PL["stop"]["gross"]["amount"] == gross_PL
+
+    price_range_DE = data["pricingDE"]["priceRange"]
+    price_range_undiscounted_DE = data["pricingDE"]["priceRangeUndiscounted"]
+    assert price_range_DE["start"]["net"]["amount"] == net_DE
+    assert price_range_DE["start"]["gross"]["amount"] == gross_DE
+    assert price_range_DE["stop"]["net"]["amount"] == net_DE
+    assert price_range_DE["stop"]["gross"]["amount"] == gross_DE
+    assert price_range_undiscounted_DE["start"]["net"]["amount"] == net_DE
+    assert price_range_undiscounted_DE["start"]["gross"]["amount"] == gross_DE
+    assert price_range_undiscounted_DE["stop"]["net"]["amount"] == net_DE
+    assert price_range_undiscounted_DE["stop"]["gross"]["amount"] == gross_DE
+
+
+def test_product_pricing_default_country_default_rate(
+    product_available_in_many_channels,
+    channel_PLN,
+    staff_api_client,
+    permission_manage_products,
+):
+    # given
+    product = product_available_in_many_channels
+    _enable_flat_rates(channel_PLN, True)
+    TaxClassCountryRate.objects.all().delete()
+    TaxClassCountryRate.objects.create(
+        country=channel_PLN.default_country, rate=TAX_RATE_PL
+    )
+
+    # when
+    variables = {
+        "id": graphene.Node.to_global_id("Product", product.id),
+        "channel": channel_PLN.slug,
+    }
+    response = staff_api_client.post_graphql(
+        QUERY_PRODUCT_PRICING,
+        variables,
+        permissions=(permission_manage_products,),
+        check_no_permissions=False,
+    )
+    content = get_graphql_content(response)
+
+    # then
+    data = None
+    for channel_data in content["data"]["product"]["channelListings"]:
+        if channel_data["channel"]["slug"] == channel_PLN.slug:
+            data = channel_data
+
+    variant = product.variants.first()
+    channel_listing = variant.channel_listings.filter(channel_id=channel_PLN.id).first()
+    gross = channel_listing.price_amount.quantize(Decimal(".01"))
+    net = (gross / Decimal(1 + TAX_RATE_PL / 100)).quantize(Decimal(".01"))
+    gross = float(gross)
+    net = float(net)
+
+    price_range = data["pricing"]["priceRange"]
+    price_range_undiscounted = data["pricing"]["priceRangeUndiscounted"]
+
+    assert price_range["start"]["net"]["amount"] == net
+    assert price_range["start"]["gross"]["amount"] == gross
+    assert price_range["stop"]["net"]["amount"] == net
+    assert price_range["stop"]["gross"]["amount"] == gross
+    assert price_range_undiscounted["start"]["net"]["amount"] == net
+    assert price_range_undiscounted["start"]["gross"]["amount"] == gross
+    assert price_range_undiscounted["stop"]["net"]["amount"] == net
+    assert price_range_undiscounted["stop"]["gross"]["amount"] == gross
+
+
+def test_product_pricing_use_tax_class_from_product_type(
+    product_available_in_many_channels,
+    channel_PLN,
+    staff_api_client,
+    permission_manage_products,
+):
+    # given
+    product = product_available_in_many_channels
+    _enable_flat_rates(channel_PLN, True)
+    TaxClassCountryRate.objects.all().delete()
+    product.tax_class = None
+    product.save(update_fields=["tax_class"])
+    product.product_type.tax_class.country_rates.create(
+        country=channel_PLN.default_country, rate=TAX_RATE_PL
+    )
+
+    # when
+    variables = {
+        "id": graphene.Node.to_global_id("Product", product.id),
+        "channel": channel_PLN.slug,
+    }
+    response = staff_api_client.post_graphql(
+        QUERY_PRODUCT_PRICING,
+        variables,
+        permissions=(permission_manage_products,),
+        check_no_permissions=False,
+    )
+    content = get_graphql_content(response)
+
+    # then
+    variant = product.variants.first()
+    channel_listing = variant.channel_listings.filter(channel_id=channel_PLN.id).first()
+    gross = channel_listing.price_amount.quantize(Decimal(".01"))
+    net = (gross / Decimal(1 + TAX_RATE_PL / 100)).quantize(Decimal(".01"))
+    gross = float(gross)
+    net = float(net)
+
+    data = None
+    for channel_data in content["data"]["product"]["channelListings"]:
+        if channel_data["channel"]["slug"] == channel_PLN.slug:
+            data = channel_data
+
+    price_range_PL = data["pricingPL"]["priceRange"]
+    price_range_undiscounted_PL = data["pricingPL"]["priceRangeUndiscounted"]
+    assert price_range_PL["start"]["net"]["amount"] == net
+    assert price_range_PL["start"]["gross"]["amount"] == gross
+    assert price_range_PL["stop"]["net"]["amount"] == net
+    assert price_range_PL["stop"]["gross"]["amount"] == gross
+    assert price_range_undiscounted_PL["start"]["net"]["amount"] == net
+    assert price_range_undiscounted_PL["start"]["gross"]["amount"] == gross
+    assert price_range_undiscounted_PL["stop"]["net"]["amount"] == net
+    assert price_range_undiscounted_PL["stop"]["gross"]["amount"] == gross
+
+
+def test_product_pricing_no_flat_rates_in_one_country(
+    product_available_in_many_channels,
+    channel_PLN,
+    staff_api_client,
+    permission_manage_products,
+):
+    # given
+    product = product_available_in_many_channels
+    _enable_flat_rates(channel_PLN, True)
+    _configure_tax_rates(product)
+    TaxConfigurationPerCountry.objects.filter(country="PL").update(
+        tax_calculation_strategy="TAX_APP"
+    )
+
+    # when
+    variables = {
+        "id": graphene.Node.to_global_id("Product", product.id),
+        "channel": channel_PLN.slug,
+    }
+    response = staff_api_client.post_graphql(
+        QUERY_PRODUCT_PRICING,
+        variables,
+        permissions=(permission_manage_products,),
+        check_no_permissions=False,
+    )
+    content = get_graphql_content(response)
+
+    # then
+    variant = product.variants.first()
+    channel_listing = variant.channel_listings.filter(channel_id=channel_PLN.id).first()
+    price_pl = float(channel_listing.price_amount.quantize(Decimal(".01")))
+    gross_de = channel_listing.price_amount.quantize(Decimal(".01"))
+    net_de = (gross_de / Decimal(1 + TAX_RATE_DE / 100)).quantize(Decimal(".01"))
+    gross_de = float(gross_de)
+    net_de = float(net_de)
+
+    data = None
+    for channel_data in content["data"]["product"]["channelListings"]:
+        if channel_data["channel"]["slug"] == channel_PLN.slug:
+            data = channel_data
+
+    price_range_PL = data["pricingPL"]["priceRange"]
+    price_range_undiscounted_PL = dat
